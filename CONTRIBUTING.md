2e410651302a6222a4ba62473efe2221006ee3e6
StoneT2000 <stonezt2019@gmail.com>
2023-07-19 01:30:56 -0700

    more tests and init contributing doc and pytest-xdist

---
+++ CONTRIBUTING.md
@@ -0,0 +1,23 @@
+# Contributing
+
+Thank you for your interest in contributing to ManiSkill2!
+
+## Setup and Installation
+
+We recommend using Python 3.9 to build and develop on ManiSkill2 (MS2), although we currently aim to support versions 3.8 and above. To get started you must set up a conda/mamba environment which can be done as follows
+
+```
+conda create -n "ms2_dev" "python==3.9"
+git clone https://github.com/haosulab/ManiSkill2.git
+cd ManiSkill2
+pip install -e .
+pip install pytest-xdist[psutil]
+```
+
+## Testing
+
+After you make changes, be sure to add any necessary tests to cover any new code in the `tests/` folder and run all the tests with the following command
+
+```
+pytest -n auto tests
+```
--- tests/test_envs.py
+++ tests/test_envs.py
@@ -2,7 +2,7 @@ import gymnasium as gym
 import pytest
 
 from mani_skill2.envs.sapien_env import BaseEnv
-
+import numpy as np
 ENV_IDS = [
     "LiftCube-v0",
     "PickCube-v0",
@@ -20,16 +20,36 @@ ENV_IDS = [
     "PushChair-v1",
     "MoveBucket-v1",
 ]
-
+OBS_MODES = [
+    "state_dict",
+    "state",
+    "rgbd",
+    "pointcloud",
+    "rgbd_robot_seg",
+    "pointcloud_robot_seg",
+]
 
 @pytest.mark.parametrize("env_id", ENV_IDS)
-def test_envs(env_id):
-    OBS_MODES = ["state_dict", "state", "rgbd", "pointcloud"]
-    for obs_mode in OBS_MODES:
-        env: BaseEnv = gym.make(env_id, obs_mode=obs_mode)
-        env.reset()
-        action_space = env.action_space
-        for _ in range(5):
-            env.step(action_space.sample())
-        env.close()
-        del env
+@pytest.mark.parametrize("obs_mode", OBS_MODES)
+def test_envs(env_id, obs_mode):
+    env = gym.make(env_id, obs_mode=obs_mode)
+    env.reset()
+    action_space = env.action_space
+    for _ in range(5):
+        env.step(action_space.sample())
+    env.close()
+    del env
+
+def test_env_seeded_reset():
+    env = gym.make(ENV_IDS[0])
+    obs, _ = env.reset(seed=2000)
+    for _ in range(5):
+        env.step(env.action_space.sample())
+    new_obs, _ = env.reset(seed=2000)
+    np.testing.assert_allclose(obs, new_obs, atol=1e-4)
+    
+    env.reset()
+    new_obs, _ = env.reset(seed=2000)
+    np.testing.assert_allclose(obs, new_obs, atol=1e-4)
+    env.close()
+    del env
