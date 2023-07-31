a662a854e3eae0943f541276fd266880af24a93e
StoneT2000 <stonezt2019@gmail.com>
2023-07-31 15:24:54 -0700

    Update vec_env.py

--- mani_skill2/vector/vec_env.py
+++ mani_skill2/vector/vec_env.py
@@ -462,7 +462,7 @@ def stack_obs(obs: Sequence, space: spaces.Space, buffer: Optional[np.ndarray] =
             ret[key] = stack_obs(_obs, space[key], buffer=_buffer)
         return ret
     elif isinstance(space, spaces.Box):
-        if len(np.array(obs[0]).shape) == 0: # check for 0-dimensional parameter
+        if not isinstance(obs[0], np.ndarray): # check for 0-dimensional parameter
             obs = [np.array(o)[None] for o in obs] # convert float to array and add dimension
         return np.stack(obs, out=buffer)
     else:
