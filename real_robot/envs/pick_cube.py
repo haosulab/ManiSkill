1e6e61189c04d08dc528aa56409a39d90b900fdf
cc299792458 <cc2018011855@gmail.com>
2023-08-14 18:04:36 -0700

    change path of real robot fodder

---
+++ real_robot/envs/pick_cube.py
@@ -0,0 +1,14 @@
+# import sys
+# sys.path.append('/home/chichu/Documents/Sapien/ManiSkill2-Sim2Real')
+
+from real_robot.utils.registration import register_env
+from mani_skill2.envs.pick_and_place.pick_cube import (
+    PickCubeEnv_v1
+)
+from .base_env import XArmBaseEnv
+
+
+@register_env("PickCubeRealXArm7", max_episode_steps=50)
+class PickCubeRealEnv(XArmBaseEnv, PickCubeEnv_v1):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
