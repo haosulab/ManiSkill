b997ab9f276fc7313c2cfdb977679245cd2c8f7e
Stone Tao <stonezt2019@gmail.com>
2023-12-21 23:44:16 -0800

    merge updates (#183)
    - fix bug where some files were missing for franka for motion planning code
    - fix bug that required user to have ai2 assets
    - update floor tiles

--- mani_skill2/utils/building/ground.py
+++ mani_skill2/utils/building/ground.py
@@ -9,20 +9,10 @@ import sapien.render
 
 def build_tesselated_square_floor(scene: sapien.Scene, floor_width=20, altitude=0):
     ground = scene.create_actor_builder()
-    rend_mtl = sapien.render.RenderMaterial(
-        base_color=[0.06, 0.08, 0.12, 1],
-        metallic=0.0,
-        roughness=0.9,
-        specular=0.8,
-    )
-    rend_mtl.diffuse_texture = sapien.render.RenderTexture2D(
-        osp.join(osp.dirname(__file__), "assets/floor_texture_4.png")
-    )
     ground.add_visual_from_file(
-        osp.join(osp.dirname(__file__), "assets/tiled_floor.obj"),
+        osp.join(osp.dirname(__file__), "assets/floor_tiles_06_2k.glb"),
         pose=sapien.Pose(p=[0, 0, altitude], q=[0.7071068, 0.7071068, 0, 0]),
         scale=[floor_width, 1, floor_width],
-        material=rend_mtl,
     )
     ground.add_plane_collision(
         sapien.Pose(p=[0, 0, altitude], q=[0.7071068, 0, -0.7071068, 0]),
