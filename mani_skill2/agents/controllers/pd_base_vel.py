e9717ca92acf5687105e4b937b56aa1d921be44f
Stone Tao <stonezt2019@gmail.com>
2024-01-23 18:12:11 -0800

    Squashed commit of the following:
    commit 27101d4bdc4bd98451ed38d209045b36794288a9
    Author: Stone Tao <stonezt2019@gmail.com>
    Date:   Tue Jan 23 18:12:03 2024 -0800
    
        fix some bugs with fetch

--- mani_skill2/agents/controllers/pd_base_vel.py
+++ mani_skill2/agents/controllers/pd_base_vel.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 
 from mani_skill2.utils.geometry import rotate_2d_vec_by_angle
+from mani_skill2.utils.structs.types import Array
 
 from .pd_joint_vel import PDJointVelController, PDJointVelControllerConfig
 
@@ -16,25 +17,18 @@ class PDBaseVelController(PDJointVelController):
         assert len(self.joints) >= 3, len(self.joints)
         super()._initialize_action_space()
 
-    def set_action(self, action: np.ndarray):
+    def set_action(self, action: Array):
         action = self._preprocess_action(action)
-
-        # TODO (arth): add support for batched qpos and gpu sim
-        if isinstance(self.qpos, torch.Tensor):
-            qpos = self.qpos.detach().cpu().numpy()
-        qpos = qpos[0]
-        if isinstance(action, torch.Tensor):
-            action = action.detach().cpu().numpy()
-
         # Convert to ego-centric action
         # Assume the 3rd DoF stands for orientation
-        ori = qpos[2]
-        vel = rotate_2d_vec_by_angle(action[:2], ori)
-        new_action = np.hstack([vel, action[2:]])
-
-        for i, joint in enumerate(self.joints):
-            joint.set_drive_velocity_target(np.array([new_action[i]]))
-
-
+        ori = self.qpos[:, 2]
+        rot_mat = torch.zeros(ori.shape[0], 2, 2, device=action.device)
+        rot_mat[:, 0, 0] = torch.cos(ori)
+        rot_mat[:, 0, 1] = -torch.sin(ori)
+        rot_mat[:, 1, 0] = torch.sin(ori)
+        rot_mat[:, 1, 1] = torch.cos(ori)
+        vel = (rot_mat @ action[:, :2].unsqueeze(-1)).squeeze(-1)
+        new_action = torch.hstack([vel, action[:, 2:]])
+        self.articulation.set_joint_drive_velocity_targets(new_action, self.joints)
 class PDBaseVelControllerConfig(PDJointVelControllerConfig):
     controller_cls = PDBaseVelController
--- manualtest/visual_fetch.py
+++ manualtest/visual_fetch.py
@@ -7,7 +7,7 @@ from mani_skill2.utils.wrappers import RecordEpisode
 
 if __name__ == "__main__":
     # , "StackCube-v0", "LiftCube-v0"
-    num_envs = 1
+    num_envs = 2
     for env_id in ["PushObject-v0"]:  # , "StackCube-v0", "LiftCube-v0"]:
         env = gym.make(
             env_id,
@@ -16,7 +16,7 @@ if __name__ == "__main__":
             robot_uid="fetch",
             reward_mode="normalized_dense",
             render_mode="rgb_array",
-            control_mode="pd_joint_delta_pos",
+            control_mode="pd_ee_delta_pos",
             sim_freq=500,
             control_freq=100,
         )
@@ -35,8 +35,14 @@ if __name__ == "__main__":
             viewer = env.render_human()
             viewer.paused = True
             env.render_human()
+        print(env.action_space.shape)
         while i < 50 or (i < 50000 and num_envs == 1):
-            obs, rew, terminated, truncated, info = env.step(env.action_space.sample())
+            action = env.action_space.sample()
+            # action[:] = 0
+            # 3 (pos), 1 (gripper), head pan, head tilt, torso lift, xy base move, rotation
+            # action[-4] = 1
+            # action[1] = -1
+            obs, rew, terminated, truncated, info = env.step(action)
             done = np.logical_or(to_numpy(terminated), to_numpy(truncated))
             if num_envs == 1:
                 env.render_human()
