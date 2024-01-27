2db8342b45130b025abd5d5d41287da9ca6252d8
Stone Tao <stonezt2019@gmail.com>
2024-01-26 17:50:09 -0800

    Update __init__.py

--- mani_skill2/envs/tasks/__init__.py
+++ mani_skill2/envs/tasks/__init__.py
@@ -3,5 +3,4 @@ from .open_cabinet_drawer import OpenCabinetEnv
 from .pick_cube import PickCubeEnv
 from .pick_single_ycb import PickSingleYCBEnv
 from .push_cube import PushCubeEnv
-from .push_object import PushObjectEnv
 from .stack_cube import StackCubeEnv
