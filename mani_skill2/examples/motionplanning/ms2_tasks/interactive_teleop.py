9c1e50d61b87ad2ffc008448f796cb078b390ff2
StoneT2000 <stonezt2019@gmail.com>
2024-02-09 00:00:07 -0800

    init code

--- mani_skill2/examples/motionplanning/ms2_tasks/interactive_teleop.py
+++ mani_skill2/examples/motionplanning/ms2_tasks/interactive_teleop.py
@@ -1,20 +1,31 @@
+import argparse
 import gymnasium as gym
 import numpy as np
 import sapien.core as sapien
 from tqdm import tqdm
+from mani_skill2.envs.sapien_env import BaseEnv
 
 from mani_skill2.envs.tasks.pick_cube import PickCubeEnv
 from mani_skill2.examples.motionplanning.motionplanner import \
     PandaArmMotionPlanningSolver
+import sapien.utils.viewer
 
-def main():
-    env: PickCubeEnv = gym.make(
-        "PickCube-v1",
+from mani_skill2.utils.wrappers.record import RecordEpisode
+def main(args):
+    env = gym.make(
+        args.env_id,
         obs_mode="none",
         control_mode="pd_joint_pos",
         render_mode="rgb_array",
         reward_mode="sparse",
         # shader_dir="rt-fast",
+    )
+    env = RecordEpisode(
+        env,
+        output_dir=f"videos/teleop-{args.env_id}",
+        trajectory_name="trajectory",
+        save_video=True,
+        info_on_video=False,
     )
     for seed in tqdm(range(100)):
         res = solve(env, seed=seed, debug=False, vis=True)
@@ -22,7 +33,7 @@ def main():
     env.close()
 
 
-def solve(env: PickCubeEnv, seed=None, debug=False, vis=False):
+def solve(env: BaseEnv, seed=None, debug=False, vis=False):
     env.reset(seed=seed)
     assert env.unwrapped.control_mode in [
         "pd_joint_pos",
@@ -39,13 +50,33 @@ def solve(env: PickCubeEnv, seed=None, debug=False, vis=False):
         joint_vel_limits=0.5,
     )
 
-    env = env.unwrapped
-    env.render_human()
-    viewer = env._viewer
-    viewer.selected_entity = planner.grasp_pose_visual._objs[0]
+    viewer = env.render_human()
+
+    last_checkpoint_state = None
+
     while True:
+        viewer.select_entity(planner.grasp_pose_visual._objs[0])
+        transform_window = viewer.plugins[0]
+
+        transform_window: sapien.utils.viewer.viewer.TransformWindow
+        transform_window.enabled = True
+        # transform_window.update_ghost_objects
+        # print(transform_window.ghost_objects, transform_window._gizmo_pose)
+        planner.grasp_pose_visual.set_pose(transform_window._gizmo_pose)
+
         env.render_human()
         execute_current_pose = False
+        if viewer.window.key_press("k"):
+            print("Saving checkpoint")
+            last_checkpoint_state = env.get_state()
+        elif viewer.window.key_press("l"):
+            if last_checkpoint_state is not None:
+                print("Loading previous checkpoint")
+                env.set_state(last_checkpoint_state)
+            else:
+                print("Could not find previous checkpoint")
+        elif viewer.window.key_press("b"):
+            env.reset()
         if viewer.window.key_press("n"):
             execute_current_pose = True
         elif viewer.window.key_press("g"):
@@ -69,42 +100,22 @@ def solve(env: PickCubeEnv, seed=None, debug=False, vis=False):
             result = planner.move_to_pose_with_screw(planner.grasp_pose_visual.pose.sp)
             execute_current_pose = False
             print(f"Reward: {result[1]}, Info: {result[-1]}")
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--env-id", type=str, default="PickCube-v1")
+    # parser.add_argument("-c", "--control-mode", type=str, default="pd_ee_delta_pose")
+    # parser.add_argument("--render-mode", type=str, default="cameras")
+    # parser.add_argument("--enable-sapien-viewer", action="store_true")
+    # parser.add_argument("--record-dir", type=str)
+    args, opts = parser.parse_known_args()
 
+    # # Parse env kwargs
+    # print("opts:", opts)
+    # eval_str = lambda x: eval(x[1:]) if x.startswith("@") else x
+    # env_kwargs = dict((x, eval_str(y)) for x, y in zip(opts[0::2], opts[1::2]))
+    # print("env_kwargs:", env_kwargs)
+    # args.env_kwargs = env_kwargs
 
-    # obb = get_actor_obb(env.cube._objs[0])
-
-    # approaching = np.array([0, 0, -1])
-    # target_closing = env.agent.tcp._objs[0].entity_pose.to_transformation_matrix()[:3, 1]
-    # grasp_info = compute_grasp_info_by_obb(
-    #     obb,
-    #     approaching=approaching,
-    #     target_closing=target_closing,
-    #     depth=FINGER_LENGTH,
-    # )
-    closing, center = grasp_info["closing"], grasp_info["center"]
-    grasp_pose = env.agent.build_grasp_pose(approaching, closing, center)
-
-    # -------------------------------------------------------------------------- #
-    # Reach
-    # -------------------------------------------------------------------------- #
-    reach_pose = grasp_pose * sapien.Pose([0, 0, -0.05])
-    planner.move_to_pose_with_screw(reach_pose)
-
-    # -------------------------------------------------------------------------- #
-    # Grasp
-    # -------------------------------------------------------------------------- #
-    planner.move_to_pose_with_screw(grasp_pose)
-    planner.close_gripper()
-
-    # -------------------------------------------------------------------------- #
-    # Move to goal pose
-    # -------------------------------------------------------------------------- #
-    goal_pose = sapien.Pose(env.goal_site.pose.p[0], grasp_pose.q)
-    res = planner.move_to_pose_with_screw(goal_pose)
-
-    planner.close()
-    return res
-
-
+    return args
 if __name__ == "__main__":
-    main()
+    main(parse_args())
