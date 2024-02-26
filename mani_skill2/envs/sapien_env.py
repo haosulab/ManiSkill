1683a0142903ea540c10fd6d122f1e0e889e2876
Stone Tao <stonezt2019@gmail.com>
2024-02-26 13:38:47 -0800

    fix bug where viewer camera was fixed in place to a sensor

--- mani_skill2/envs/sapien_env.py
+++ mani_skill2/envs/sapien_env.py
@@ -956,15 +956,16 @@ class BaseEnv(gym.Env):
         )
         control_window.show_joint_axes = False
         control_window.show_camera_linesets = False
+        if "render_camera" in self._human_render_cameras:
+            self._viewer.set_camera_pose(
+                self._human_render_cameras["render_camera"].camera.global_pose
+            )
 
     def render_human(self):
         if self._viewer is None:
             self._viewer = Viewer()
             self._setup_viewer()
-        if "render_camera" in self._human_render_cameras:
-            self._viewer.set_camera_pose(
-                self._human_render_cameras["render_camera"].camera.global_pose
-            )
+
         for obj in self._hidden_objects:
             obj.show_visual()
         if physx.is_gpu_enabled() and self._scene._gpu_sim_initialized:
