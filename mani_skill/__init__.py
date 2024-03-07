fade19380e3c281641d6befdeca04570c4804d62
Stone Tao <stonezt2019@gmail.com>
2024-03-07 13:38:31 -0800

    work

--- mani_skill/__init__.py
+++ mani_skill/__init__.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from .utils.logging_utils import logger
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 # ---------------------------------------------------------------------------- #
 # Setup paths
