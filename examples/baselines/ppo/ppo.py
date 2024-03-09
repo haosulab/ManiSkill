d21e64447595b93f7e3fbf1f8d85e94c74af2143
chenbao <im.b.c@live.com>
2024-03-09 16:02:36 -0500

    update rotate_cube v1

--- examples/baselines/ppo/ppo.py
+++ examples/baselines/ppo/ppo.py
@@ -51,7 +51,7 @@ class Args:
     """total timesteps of the experiments"""
     learning_rate: float = 3e-4
     """the learning rate of the optimizer"""
-    num_envs: int = 512
+    num_envs: int = 5120
     """the number of parallel environments"""
     num_eval_envs: int = 8
     """the number of parallel evaluation environments"""
