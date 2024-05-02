b6ee9753f3ad458ac538d9e1cdce760ad9b23f59
StoneT2000 <stonezt2019@gmail.com>
2024-05-02 05:30:52 -0700

    google robot and widowx robot

--- mani_skill/agents/robots/__init__.py
+++ mani_skill/agents/robots/__init__.py
@@ -1,13 +1,15 @@
-from .allegro_hand import AllegroHandLeft, AllegroHandRight, AllegroHandRightTouch
+from .allegro_hand import *
 from .anymal import ANYmalC
 from .dclaw import DClaw
 from .fetch import Fetch
+from .googlerobot import *
 from .humanoid import Humanoid
 from .panda import *
 from .stompy import Stompy
 from .trifingerpro import TriFingerPro
 from .unitree_go import *
-from .unitree_h1 import UnitreeH1, UnitreeH1Simplified
+from .unitree_h1 import *
 from .ur_e import UR10e
+from .widowx import *
 from .xarm import XArm7Ability
 from .xmate3 import Xmate3Robotiq
---
+++ mani_skill/agents/robots/widowx/widowx.py
@@ -0,0 +1,16 @@
+import numpy as np
+import sapien
+
+from mani_skill import ASSET_DIR
+from mani_skill.agents.base_agent import BaseAgent
+from mani_skill.agents.controllers import *
+from mani_skill.agents.registration import register_agent
+from mani_skill.sensors.camera import CameraConfig
+
+
+# TODO (stao) (xuanlin): model it properly based on real2sim
+@register_agent()
+class WidowX250S(BaseAgent):
+    uid = "widowx250s"
+    urdf_path = f"{ASSET_DIR}/robots/widowx250s/wx250s.urdf"
+    urdf_config = dict()
