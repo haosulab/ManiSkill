dfcaf03d6ec1fb1c6f62f9549111c905456d0f0d
Filip Grigorov <filip.grigorov.email@gmail.com>
2024-05-18 20:06:15 -0400

    - Changed the mapping of the port for the tensorboard logs

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,3 +1,4 @@
 NAME=$1
 
-docker run --gpus all -p 8888:8888 -p 6006:6006 -v $(pwd):/home/filip-grigorov/code -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -it nvcr.io/filip.grigorov/$NAME
+docker run --gpus all -p 8888:8888 -p 16006:6006 -v $(pwd):/home/filip-grigorov/code -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -it nvcr.io/filip.grigorov/$NAME
+
