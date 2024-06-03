fca436aec27f7912637c38d2a90f2cae2dee5e4c
szczekulskij <szczekulskij@gmail.com>
2024-06-02 20:38:15 -0700

    add bottle

--- mani_skill/envs/tasks/tabletop/pull_cube_with_hockey_stick.py
+++ mani_skill/envs/tasks/tabletop/pull_cube_with_hockey_stick.py
@@ -19,18 +19,53 @@ from typing import Any, Dict, Union
 
 import numpy as np
 import torch
+import sapien
 import torch.random
 from transforms3d.euler import euler2quat
 
 from mani_skill.agents.robots import Fetch, Panda, Xmate3Robotiq
 from mani_skill.envs.sapien_env import BaseEnv
 from mani_skill.sensors.camera import CameraConfig
+from mani_skill.envs.scene import ManiSkillScene
 from mani_skill.utils import common, sapien_utils
 from mani_skill.utils.building import actors
 from mani_skill.utils.registration import register_env
 from mani_skill.utils.scene_builder.table import TableSceneBuilder
 from mani_skill.utils.structs import Pose
-from mani_skill.utils.structs.types import Array, GPUMemoryConfig, SimConfig
+from mani_skill.utils.structs.types import Array
+
+
+
+
+
+# _stick_length = 2
+# _stick_end_length = 1
+# _stick_thickness = 1e-2
+
+# def _build_hockey_stick(
+#     scene: ManiSkillScene, 
+#     stick_length: float, 
+#     end_of_stick_length: float,
+# ):
+#     builder = scene.create_actor_builder()
+
+#     # stick
+#     material = sapien.render.RenderMaterial(
+#         base_color=sapien_utils.hex2rgba("#FFD289"), roughness=0.5, specular=0.5
+#     )
+
+#     builder.add_box_collision()
+
+
+
+#     # end of stick (another stick)
+
+from mani_skill.utils.building import articulations
+
+
+
+
+
 
 
 @register_env("PullCubeWithHockeyStick-v1", max_episode_steps=50)
@@ -82,41 +117,41 @@ class PullCubeWithHockeyStickEnv(BaseEnv):
             name="cube",
             body_type="dynamic",
         )
+        model_id = "1349b2169a97a0ff54e1b6f41fdd78a"
+        builder = articulations.get_articulation_builder(
+            self.scene, f"partnet-mobility:{model_id}"
+            )
+        self.bottle = builder.build(name="bottle")
 
         
 
     def _initialize_episode(self, env_idx: torch.Tensor, options: dict):
-        # use the torch.device context manager to automatically create tensors on CPU or CUDA depending on self.device, the device the environment runs on
         with torch.device(self.device):
-            # the initialization functions where you as a user place all the objects and initialize their properties
-            # are designed to support partial resets, where you generate initial state for a subset of the environments.
-            # this is done by using the env_idx variable, which also tells you the batch size
+            # initial setup for handling multiple environments
             b = len(env_idx)
-            # when using scene builders, you must always call .initialize on them so they can set the correct poses of objects in the prebuilt scene
-            # note that the table scene is built such that z=0 is the surface of the table.
             self.table_scene.initialize(env_idx)
 
-            # here we write some randomization code that randomizes the x, y position of the cube we are pushing in the range [-0.1, -0.1] to [0.1, 0.1]
+            # set the cube's initial position
             xyz = torch.zeros((b, 3))
             xyz[..., :2] = torch.rand((b, 2)) * 0.2 - 0.1
             xyz[..., 2] = self.cube_half_size
             q = [1, 0, 0, 0]
-            # we can then create a pose object using Pose.create_from_pq to then set the cube pose with. Note that even though our quaternion
-            # is not batched, Pose.create_from_pq will automatically batch p or q accordingly
-            # furthermore, notice how here we do not even using env_idx as a variable to say set the pose for objects in desired
-            # environments. This is because internally any calls to set data on the GPU buffer (e.g. set_pose, set_linear_velocity etc.)
-            # automatically are masked so that you can only set data on objects in environments that are meant to be initialized
             obj_pose = Pose.create_from_pq(p=xyz, q=q)
             self.obj.set_pose(obj_pose)
 
-            # here we set the location of that red/white target (the goal region). In particular here, we set the position to be in front of the cube
-            # and we further rotate 90 degrees on the y-axis to make the target object face up
+            # set the bottle's initial position
+            print("self.bottle: ", self.bottle)
+            print("self.bottle attributes: ", dir(self.bottle))
             target_region_xyz = xyz + torch.tensor([0.1 + self.goal_radius, 0, 0])
-            # set a little bit above 0 so the target is sitting on the table
             target_region_xyz[..., 2] = 1e-3
+            self.bottle.set_pose(
+                Pose.create_from_pq(
+                    p=target_region_xyz,
+                    q=euler2quat(0, np.pi / 2, 0),
+                )
+            )
 
     def evaluate(self):
-
         return {
             "success": True,
         }
