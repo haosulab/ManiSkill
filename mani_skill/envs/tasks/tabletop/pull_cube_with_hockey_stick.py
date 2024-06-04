81817deb902233ae785ad20d411e48c015416ad1
szczekulskij <szczekulskij@gmail.com>
2024-06-03 19:19:37 -0700

    mock change

--- mani_skill/envs/tasks/tabletop/pull_cube_with_hockey_stick.py
+++ mani_skill/envs/tasks/tabletop/pull_cube_with_hockey_stick.py
@@ -218,11 +218,11 @@ class PullCubeWithHockeyStickEnv(BaseEnv):
             )
 
             # set grasp stick region to appear over the end of the stick
-            offset = torch.tensor([
-                -(_stick_length - _stick_thickness),
-                -(_stick_end_length - _stick_thickness),
-                0])
-            target_region_xyz = torch.tensor(self.hockey_stick.pose.p) + offset
+            # offset = torch.tensor([
+            #     -(_stick_length - _stick_thickness),
+            #     -(_stick_end_length - _stick_thickness),
+            #     0])
+            target_region_xyz = torch.tensor(self.hockey_stick.pose.p) # + offset
             target_region_xyz[..., 2] = 4 * _stick_thickness
             self.grasp_stick_region.set_pose(
                 Pose.create_from_pq(
