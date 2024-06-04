32c813e921857589c2679e871ac3fe8ba06cd556
szczekulskij <szczekulskij@gmail.com>
2024-06-03 21:04:07 -0700

    add evaluate back in

--- mani_skill/envs/tasks/tabletop/pull_cube_with_hockey_stick.py
+++ mani_skill/envs/tasks/tabletop/pull_cube_with_hockey_stick.py
@@ -268,12 +268,11 @@ class PullCubeWithHockeyStickEnv(BaseEnv):
         print("type(dst_cube_to_end_of_stick):", type(dst_cube_to_end_of_stick))
         print("type(dst_robot_to_grasp_stick_pos):", type(dst_robot_to_grasp_stick_pos))
 
-        return dst_cube_to_end_of_stick, dst_robot_to_grasp_stick_pos
+        return dst_cube_to_end_of_stick.to(self.device), dst_robot_to_grasp_stick_pos.to(self.device)
 
 
 
     def evaluate(self):
-        a = self._get_pos_of_end_of_stick()
 
 
         is_obj_in_goal = (
@@ -285,15 +284,15 @@ class PullCubeWithHockeyStickEnv(BaseEnv):
         is_grasped = self.agent.is_grasping(self.hockey_stick)
         is_robot_static = self.agent.is_static(0.2)
 
-        # dst_cube_to_end_of_stick, dst_robot_to_grasp_stick_pos = self._get_distances()
+        dst_cube_to_end_of_stick, dst_robot_to_grasp_stick_pos = self._get_distances()
 
         return {
             "success": is_obj_in_goal & is_robot_static,
             "is_obj_in_goal": is_obj_in_goal,
             "is_robot_static": is_robot_static,
             "is_grasped": is_grasped,
-            # "dst_cube_to_end_of_stick": dst_cube_to_end_of_stick,
-            # "dst_robot_to_grasp_stick_pos": dst_robot_to_grasp_stick_pos,
+            "dst_cube_to_end_of_stick": dst_cube_to_end_of_stick,
+            "dst_robot_to_grasp_stick_pos": dst_robot_to_grasp_stick_pos,
         }
 
     def _get_obs_extra(self, info: Dict):
