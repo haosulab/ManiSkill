2342b7762d95d8995fb227a05c73c5973bee90e7
szczekulskij <szczekulskij@gmail.com>
2024-06-05 16:49:59 -0700

    run pre-commit

--- mani_skill/envs/tasks/tabletop/pull_cube_with_hockey_stick.py
+++ mani_skill/envs/tasks/tabletop/pull_cube_with_hockey_stick.py
@@ -18,15 +18,15 @@ See comments for how to make your own environment and what each required functio
 from typing import Any, Dict, Union
 
 import numpy as np
-import torch
 import sapien
+import torch
 import torch.random
 from transforms3d.euler import euler2quat
 
 from mani_skill.agents.robots import Fetch, Panda, Xmate3Robotiq
 from mani_skill.envs.sapien_env import BaseEnv
-from mani_skill.sensors.camera import CameraConfig
 from mani_skill.envs.scene import ManiSkillScene
+from mani_skill.sensors.camera import CameraConfig
 from mani_skill.utils import common, sapien_utils
 from mani_skill.utils.building import actors
 from mani_skill.utils.registration import register_env
@@ -34,18 +34,15 @@ from mani_skill.utils.scene_builder.table import TableSceneBuilder
 from mani_skill.utils.structs import Pose
 from mani_skill.utils.structs.types import Array
 
-
-
-
-
 _stick_length = 0.2
 _stick_end_length = 0.1
-_stick_thickness = 5e-3 # y & z thicky
+_stick_thickness = 5e-3  # y & z thicky
 goal_thresh = 0.025
 
+
 def _build_hockey_stick(
-    scene: ManiSkillScene, 
-    stick_length: float, 
+    scene: ManiSkillScene,
+    stick_length: float,
     end_of_stick_length: float,
     stick_thickness: float,
 ):
@@ -56,13 +53,15 @@ def _build_hockey_stick(
     )
 
     half_sizes = [
-        [stick_length, stick_thickness, stick_thickness], # long stick
-        [stick_thickness, end_of_stick_length, stick_thickness], # end of stick
+        [stick_length, stick_thickness, stick_thickness],  # long stick
+        [stick_thickness, end_of_stick_length, stick_thickness],  # end of stick
     ]
 
     poses = [
         sapien.Pose(p=[0, 0, 0]),
-        sapien.Pose(p=[stick_length+ stick_thickness , end_of_stick_length - stick_thickness, 0]),
+        sapien.Pose(
+            p=[stick_length + stick_thickness, end_of_stick_length - stick_thickness, 0]
+        ),
     ]
     for pose, half_size in zip(poses, half_sizes):
         builder.add_box_collision(pose, half_size)
@@ -83,19 +82,16 @@ class PullCubeWithHockeyStickEnv(BaseEnv):
     goal_radius = 0.1
     cube_half_size = 0.02
 
-
     # same as pick_cube, stack_cube and push_cube
     def __init__(self, *args, robot_uids="panda", robot_init_qpos_noise=0.02, **kwargs):
         self.robot_init_qpos_noise = robot_init_qpos_noise
         super().__init__(*args, robot_uids=robot_uids, **kwargs)
 
-
     # same as pick_cube, stack_cube and push_cube
-    @property 
+    @property
     def _default_sensor_configs(self):
         pose = sapien_utils.look_at(eye=[0.3, 0, 0.6], target=[-0.1, 0, 0.1])
         return [CameraConfig("base_camera", pose, 128, 128, np.pi / 2, 0.01, 100)]
-
 
     # same as pick_cube, stack_cube and push_cube
     @property
@@ -113,7 +109,6 @@ class PullCubeWithHockeyStickEnv(BaseEnv):
         )
         self.table_scene.build()
 
-        
         self.cube = actors.build_cube(
             self.scene,
             half_size=self.cube_half_size,
@@ -151,10 +146,11 @@ class PullCubeWithHockeyStickEnv(BaseEnv):
             obj_pose = Pose.create_from_pq(p=xyz, q=q)
             self.cube.set_pose(obj_pose)
 
-            
             # set the goal's initial position
             target_region_xyz = xyz - torch.tensor([0.1 + self.goal_radius, 0, 0])
-            target_region_xyz[..., 2] = 1e-3 # # set the z pos slightly above 0 so the target is on (not in) the table
+            target_region_xyz[
+                ..., 2
+            ] = 1e-3  # # set the z pos slightly above 0 so the target is on (not in) the table
             self.goal_region.set_pose(
                 Pose.create_from_pq(
                     p=target_region_xyz,
@@ -162,12 +158,14 @@ class PullCubeWithHockeyStickEnv(BaseEnv):
                 )
             )
 
-
             # set the stick's initial position
-            offset = torch.tensor([
-                - (_stick_length - 2 * self.cube_half_size) , 
-                - (_stick_end_length + 3* self.cube_half_size),
-                0])
+            offset = torch.tensor(
+                [
+                    -(_stick_length - 2 * self.cube_half_size),
+                    -(_stick_end_length + 3 * self.cube_half_size),
+                    0,
+                ]
+            )
             target_region_xyz = xyz + offset
             target_region_xyz[..., 2] = _stick_thickness
             self.hockey_stick.set_pose(
@@ -178,27 +176,23 @@ class PullCubeWithHockeyStickEnv(BaseEnv):
             )
 
     def _get_pos_of_end_of_stick(self):
-        '''get the middle of end the shorter stick (end of the stick)'''
-        offset = torch.tensor([
-                _stick_length + _stick_thickness,
-                _stick_end_length,
-                0]).to(self.device)
+        """get the middle of end the shorter stick (end of the stick)"""
+        offset = torch.tensor(
+            [_stick_length + _stick_thickness, _stick_end_length, 0]
+        ).to(self.device)
         return torch.tensor(self.hockey_stick.pose.p + offset).to(self.device)
-    
+
     def _get_pos_of_grasp_stick(self):
-        '''get the grasping position of the stick (3/4 of the stick length from the end of the stick)'''
-        offset = torch.tensor([
-                -_stick_length/2,
-                0,
-                0]).to(self.device)
+        """get the grasping position of the stick (3/4 of the stick length from the end of the stick)"""
+        offset = torch.tensor([-_stick_length / 2, 0, 0]).to(self.device)
         return torch.tensor(self.hockey_stick.pose.p + offset).to(self.device)
 
     def _get_distances(self):
-        '''
+        """
         return two distances:
         (1) from the end of the stick to the cube
         (2) from the robot to the grasp region
-        '''
+        """
         # calcs for (1)
         dst_cube_to_end_of_stick = torch.linalg.norm(
             torch.tensor(self.cube.pose.p) - self._get_pos_of_end_of_stick(), axis=1
@@ -209,12 +203,11 @@ class PullCubeWithHockeyStickEnv(BaseEnv):
             torch.tensor(self.agent.tcp.pose.p) - self._get_pos_of_grasp_stick(), axis=1
         )
 
-        return dst_cube_to_end_of_stick.to(self.device), dst_robot_to_grasp_stick_pos.to(self.device)
-
-
+        return dst_cube_to_end_of_stick.to(
+            self.device
+        ), dst_robot_to_grasp_stick_pos.to(self.device)
 
     def evaluate(self):
-
 
         is_obj_in_goal = (
             torch.linalg.norm(
@@ -239,14 +232,16 @@ class PullCubeWithHockeyStickEnv(BaseEnv):
     def _get_obs_extra(self, info: Dict):
         dst_cube_to_end_of_stick, dst_robot_to_grasp_stick_pos = self._get_distances()
         # default observartions
-        obs = dict(tcp_pose=self.agent.tcp.pose.raw_pose,)
+        obs = dict(
+            tcp_pose=self.agent.tcp.pose.raw_pose,
+        )
         if self._obs_mode in ["state", "state_dict"]:
             obs.update(
                 obj_pose=self.cube.pose.raw_pose,
-                dst_cube_to_end_of_stick = dst_cube_to_end_of_stick,
-                dst_robot_to_grasp_stick_pos = dst_robot_to_grasp_stick_pos,
-                stick_pose = self.hockey_stick.pose.raw_pose,
-                obj_to_goal_dist = self.goal_region.pose.p - self.cube.pose.p,
+                dst_cube_to_end_of_stick=dst_cube_to_end_of_stick,
+                dst_robot_to_grasp_stick_pos=dst_robot_to_grasp_stick_pos,
+                stick_pose=self.hockey_stick.pose.raw_pose,
+                obj_to_goal_dist=self.goal_region.pose.p - self.cube.pose.p,
             )
         return obs
 
@@ -259,7 +254,7 @@ class PullCubeWithHockeyStickEnv(BaseEnv):
 
         # 2. Add reward when we pick up the stick
         is_grasped = info["is_grasped"]
-        reward+= is_grasped
+        reward += is_grasped
 
         # 3. Add reward as the distance of the cube to the goal decreases
         obj_to_goal_dist = torch.linalg.norm(
@@ -267,17 +262,15 @@ class PullCubeWithHockeyStickEnv(BaseEnv):
         )
         place_reward = (1 - torch.tanh(5 * obj_to_goal_dist)) * is_grasped
         reward += place_reward
-        
+
         # 5. Add reward when the robot is static
         static_reward = 1 - torch.tanh(
             5 * torch.linalg.norm(self.agent.robot.get_qvel()[..., :-2], axis=1)
         )
         reward += static_reward * info["is_obj_in_goal"]
         reward[info["success"]] = 5
-        
+
         return reward
-
-
 
     def compute_normalized_dense_reward(self, obs: Any, action: Array, info: Dict):
         # this should be equal to compute_dense_reward / max possible reward
