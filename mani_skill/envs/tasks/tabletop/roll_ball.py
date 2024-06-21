8f9b64010b51c10ac4eff880384abb751f432ac1
StoneT2000 <stonezt2019@gmail.com>
2024-06-21 01:18:54 -0700

    Update roll_ball.py

--- mani_skill/envs/tasks/tabletop/roll_ball.py
+++ mani_skill/envs/tasks/tabletop/roll_ball.py
@@ -2,6 +2,7 @@ from typing import Any, Dict, Union
 
 import numpy as np
 import torch
+from transforms3d.euler import euler2quat
 
 import mani_skill.envs.utils.randomization as randomization
 from mani_skill.agents.robots import Fetch, Panda, Xmate3Robotiq
@@ -13,7 +14,7 @@ from mani_skill.utils.registration import register_env
 from mani_skill.utils.scene_builder.table import TableSceneBuilder
 from mani_skill.utils.structs.pose import Pose
 from mani_skill.utils.structs.types import Array, GPUMemoryConfig, SimConfig
-from transforms3d.euler import euler2quat
+
 
 @register_env("RollBall-v1", max_episode_steps=80)
 class RollBallEnv(BaseEnv):
@@ -36,15 +37,12 @@ class RollBallEnv(BaseEnv):
 
     agent: Union[Panda]
 
-    goal_radius: float = 0.1 # radius of the goal region
-    ball_radius: float = 0.035 # radius of the ball
+    goal_radius: float = 0.1  # radius of the goal region
+    ball_radius: float = 0.035  # radius of the ball
     reached_status: torch.Tensor
-
-
 
     def __init__(self, *args, robot_uids="panda", robot_init_qpos_noise=0.02, **kwargs):
         self.robot_init_qpos_noise = robot_init_qpos_noise
-        self.reached_status = torch.zeros(kwargs["num_envs"], dtype=torch.float32)
         super().__init__(*args, robot_uids=robot_uids, **kwargs)
 
     @property
@@ -59,12 +57,11 @@ class RollBallEnv(BaseEnv):
     def _default_sensor_configs(self):
         pose = sapien_utils.look_at(eye=[-0.1, 0.9, 0.3], target=[0.0, 0.0, 0.0])
         return [CameraConfig("base_camera", pose, 128, 128, np.pi / 2, 0.01, 100)]
-    
+
     @property
     def _default_human_render_camera_configs(self):
         pose = sapien_utils.look_at([-0.6, 1.3, 0.8], [0.0, 0.13, 0.0])
         return CameraConfig("render_camera", pose, 512, 512, 1, 0.01, 100)
-
 
     def _load_scene(self, options: dict):
         self.table_scene = TableSceneBuilder(
@@ -78,7 +75,6 @@ class RollBallEnv(BaseEnv):
             color=[0, 0.2, 0.8, 1],
             name="ball",
         )
-        
 
         self.goal_region = actors.build_red_white_target(
             self.scene,
@@ -88,6 +84,7 @@ class RollBallEnv(BaseEnv):
             add_collision=False,
             body_type="kinematic",
         )
+        self.reached_status = torch.zeros(self.num_envs, dtype=torch.float32)
 
     def _initialize_episode(self, env_idx: torch.Tensor, options: dict):
         self.reached_status = self.reached_status.to(self.device)
@@ -95,12 +92,14 @@ class RollBallEnv(BaseEnv):
             b = len(env_idx)
             self.table_scene.initialize(env_idx)
 
-            robot_pose = Pose.create_from_pq(p=[-0.1, 1.0, 0], q=[0.7071, 0, 0, -0.7072])
+            robot_pose = Pose.create_from_pq(
+                p=[-0.1, 1.0, 0], q=[0.7071, 0, 0, -0.7072]
+            )
             self.agent.robot.set_pose(robot_pose)
 
             xyz = torch.zeros((b, 3))
-            xyz[...,  0] = (torch.rand((b))*2 - 1)*0.3 - 0.1
-            xyz[..., 1] = torch.rand((b))*0.2 + 0.5
+            xyz[..., 0] = (torch.rand((b)) * 2 - 1) * 0.3 - 0.1
+            xyz[..., 1] = torch.rand((b)) * 0.2 + 0.5
             xyz[..., 2] = self.ball_radius
             q = [1, 0, 0, 0]
 
@@ -108,9 +107,9 @@ class RollBallEnv(BaseEnv):
             self.ball.set_pose(obj_pose)
 
             xyz_goal = torch.zeros((b, 3))
-            xyz_goal[...,  0] = (torch.rand((b))*2 - 1)*0.3 -0.1
-            xyz_goal[..., 1]  = torch.rand((b))*0.2 - 1.0 + self.goal_radius
-            xyz_goal[..., 2]  = 1e-3
+            xyz_goal[..., 0] = (torch.rand((b)) * 2 - 1) * 0.3 - 0.1
+            xyz_goal[..., 1] = torch.rand((b)) * 0.2 - 1.0 + self.goal_radius
+            xyz_goal[..., 2] = 1e-3
             self.goal_region.set_pose(
                 Pose.create_from_pq(
                     p=xyz_goal,
@@ -131,7 +130,7 @@ class RollBallEnv(BaseEnv):
         return {
             "success": is_obj_placed,
         }
-    
+
     def _get_obs_extra(self, info: Dict):
 
         obs = dict(
@@ -146,32 +145,33 @@ class RollBallEnv(BaseEnv):
                 ball_to_goal_pos=self.goal_region.pose.p - self.ball.pose.p,
             )
         return obs
-    
+
     def compute_dense_reward(self, obs: Any, action: Array, info: Dict):
         unit_vec = self.ball.pose.p - self.goal_region.pose.p
         unit_vec = unit_vec / torch.linalg.norm(unit_vec, axis=1, keepdim=True)
         tcp_hit_pose = Pose.create_from_pq(
-            p=self.ball.pose.p
-            + unit_vec * (self.ball_radius + 0.05),
+            p=self.ball.pose.p + unit_vec * (self.ball_radius + 0.05),
         )
         tcp_to_hit_pose = tcp_hit_pose.p - self.agent.tcp.pose.p
         tcp_to_hit_pose_dist = torch.linalg.norm(tcp_to_hit_pose, axis=1)
         self.reached_status[tcp_to_hit_pose_dist < 0.04] = 1.0
-        reaching_reward = (1 - torch.tanh(2*tcp_to_hit_pose_dist))
+        reaching_reward = 1 - torch.tanh(2 * tcp_to_hit_pose_dist)
 
         obj_to_goal_dist = torch.linalg.norm(
             self.ball.pose.p[..., :2] - self.goal_region.pose.p[..., :2], axis=1
         )
-        
-        reached_reward = (1-torch.tanh(obj_to_goal_dist))
 
-        reward = 20*reached_reward*self.reached_status\
-              + reaching_reward*(1-self.reached_status)\
+        reached_reward = 1 - torch.tanh(obj_to_goal_dist)
+
+        reward = (
+            20 * reached_reward * self.reached_status
+            + reaching_reward * (1 - self.reached_status)
             + self.reached_status
-        
+        )
+
         reward[info["success"]] = 30.0
         return reward
-    
+
     def compute_normalized_dense_reward(self, obs: Any, action: Array, info: Dict):
         max_reward = 30.0
         return self.compute_dense_reward(obs=obs, action=action, info=info) / max_reward
