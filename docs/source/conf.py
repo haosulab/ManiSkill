ffc01c3ba4076d7335163d2edfd5b7e97ad2cdb1
Stone Tao <stonezt2019@gmail.com>
2024-06-22 18:50:51 -0700

    [VersionBump] v3.0.0b5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath("../../mani_skill"))
-__version__ = "3.0.0b4"
+__version__ = "3.0.0b5"
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
