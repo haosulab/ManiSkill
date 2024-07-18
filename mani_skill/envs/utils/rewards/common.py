03060c7e10168a34b6e46fd5bbfbf70bf1d11d43
Xander <xhinrichsen@ucsd.edu>
2024-07-18 04:02:18 -0700

    sigmoidal rewards comment removed

--- mani_skill/envs/utils/rewards/common.py
+++ mani_skill/envs/utils/rewards/common.py
@@ -1,5 +1,6 @@
-import torch
 import numpy as np
+import torch
+
 
 def tolerance(
     x, lower=0.0, upper=0.0, margin=0.0, sigmoid="gaussian", value_at_margin=0.1
@@ -23,7 +24,7 @@ def tolerance(
          'linear', 'hyperbolic', 'long_tail', 'cosine', 'tanh_squared'.
       value_at_margin: A float between 0 and 1 specifying the output value when
         the distance from `x` to the nearest bound is equal to `margin`. Ignored
-        if `margin == 0`. todo: not implemented yet
+        if `margin == 0`.
 
     Returns:
       A torch array with values between 0.0 and 1.0.
@@ -31,20 +32,24 @@ def tolerance(
     Raises:
       ValueError: If `bounds[0] > bounds[1]`.
       ValueError: If `margin` is negative.
-      ValueError: If not 0 < `value_at_margin` < 1, 
+      ValueError: If not 0 < `value_at_margin` < 1,
       except for `linear`, `cosine` and `quadratic` sigmoids, which allow `value_at_margin` == 0.
       ValueError: If `sigmoid` is of an unknown type.
     """
 
-    if sigmoid in ('cosine', 'linear', 'quadratic'):
-      if not 0 <= value_at_margin < 1:
-        raise ValueError('`value_at_margin` must be nonnegative and smaller than 1, '
-                          'got {}.'.format(value_at_margin))
+    if sigmoid in ("cosine", "linear", "quadratic"):
+        if not 0 <= value_at_margin < 1:
+            raise ValueError(
+                "`value_at_margin` must be nonnegative and smaller than 1, "
+                "got {}.".format(value_at_margin)
+            )
     else:
-      if not 0 < value_at_margin < 1:
-        raise ValueError('`value_at_margin` must be strictly between 0 and 1, '
-                          'got {}.'.format(value_at_margin))
-    
+        if not 0 < value_at_margin < 1:
+            raise ValueError(
+                "`value_at_margin` must be strictly between 0 and 1, "
+                "got {}.".format(value_at_margin)
+            )
+
     if lower > upper:
         raise ValueError("Lower bound must be <= upper bound.")
 
@@ -60,19 +65,21 @@ def tolerance(
         if sigmoid == "gaussian":
             scale = np.sqrt(-2 * np.log(value_at_margin))
             value = torch.where(
-                in_bounds, torch.tensor(1.0), torch.exp(-0.5 * (d*scale)**2)
+                in_bounds, torch.tensor(1.0), torch.exp(-0.5 * (d * scale) ** 2)
             )
         elif sigmoid == "hyperbolic":
-            scale = np.arccosh(1/value_at_margin)
-            value = torch.where(in_bounds, torch.tensor(1.0), 1 / (1 + torch.exp(d*scale)))
+            scale = np.arccosh(1 / value_at_margin)
+            value = torch.where(
+                in_bounds, torch.tensor(1.0), 1 / (1 + torch.exp(d * scale))
+            )
         elif sigmoid == "quadratic":
-            scale = np.sqrt(1-value_at_margin)
-            scaled_d = d*scale
+            scale = np.sqrt(1 - value_at_margin)
+            scaled_d = d * scale
             x = torch.where(scaled_d.abs() < 1, 1 - scaled_d**2, torch.tensor(0.0))
             value = torch.where(in_bounds, torch.tensor(1.0), x)
         elif sigmoid == "linear":
-            scale = 1-value_at_margin
-            scaled_d = d*scale
+            scale = 1 - value_at_margin
+            scaled_d = d * scale
             x = torch.where(scaled_d.abs() < 1, 1 - scaled_d, torch.tensor(0.0))
             value = torch.where(in_bounds, torch.tensor(1.0), x)
         else:
