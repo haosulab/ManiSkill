18cf9d02abb6b46441cfae600716c29e03345a66
StoneT2000 <stonezt2019@gmail.com>
2024-07-31 18:41:32 -0700

    work

--- examples/baselines/ppo/baselines.sh
+++ examples/baselines/ppo/baselines.sh
@@ -7,7 +7,7 @@ seeds=(9351 4796 1788)
 for seed in ${seeds[@]}
 do
   python ppo.py --env_id="PushCube-v1" --seed=${seed} \
-    --num_envs=2048 --update_epochs=8 --num_minibatches=32 \
+    --num_envs=2048 --update_epochs=8 --num_minibatches=32 --reward_scale=0.1 \
     --total_timesteps=50_000_000 --eval_freq=10 --num-steps=20 \
     --no_partial_reset --reconfiguration_freq=1 \
     --exp-name="ppo-PushCube-v1-state-${seed}-walltime_efficient" \
@@ -17,7 +17,7 @@ done
 for seed in ${seeds[@]}
 do
   python ppo.py --env_id="PickCube-v1" --seed=${seed} \
-    --num_envs=1024 --update_epochs=8 --num_minibatches=32 \
+    --num_envs=1024 --update_epochs=8 --num_minibatches=32 --reward_scale=0.1 \
     --total_timesteps=50_000_000 \
     --no_partial_reset --reconfiguration_freq=1 \
     --exp-name="ppo-PickCube-v1-state-${seed}-walltime_efficient" \
@@ -27,7 +27,7 @@ done
 for seed in ${seeds[@]}
 do
   python ppo.py --env_id="PickSingleYCB-v1" --seed=${seed} \
-    --num_envs=1024 --update_epochs=8 --num_minibatches=32 \
+    --num_envs=1024 --update_epochs=8 --num_minibatches=32 --reward_scale=0.1 \
     --total_timesteps=50_000_000 \
     --no_partial_reset --reconfiguration_freq=1 \
     --exp-name="ppo-PickSingleYCB-v1-state-${seed}-walltime_efficient" \
@@ -37,8 +37,8 @@ done
 for seed in ${seeds[@]}
 do
   python ppo.py --env_id="PushT-v1" --seed=${seed} \
-    --num_envs=1024 --update_epochs=8 --num_minibatches=32 \
-    --total_timesteps=50_000_000 --num-steps=100 --num_eval_steps=100 --gamma=0.99 --reward_scale=0.1 \
+    --num_envs=1024 --update_epochs=8 --num_minibatches=32 --gamma=0.99 --reward_scale=0.1 \
+    --total_timesteps=50_000_000 --num-steps=100 --num_eval_steps=100 \
     --no_partial_reset --reconfiguration_freq=1 \
     --exp-name="ppo-PushT-v1-state-${seed}-walltime_efficient" \
     --wandb_entity="stonet2000" --track
@@ -46,10 +46,9 @@ done
 
 for seed in ${seeds[@]}
 do
-  python ppo.py --env_id="AnymalC-Reach-v1" \
-    --num_envs=1024 --update_epochs=8 --num_minibatches=32 \
+  python ppo.py --env_id="AnymalC-Reach-v1" --seed=${seed} \
+    --num_envs=1024 --update_epochs=8 --num_minibatches=32 --gamma=0.99 --gae_lambda=0.95 --reward_scale=0.1 \
     --total_timesteps=50_000_000 --num-steps=200 --num-eval-steps=200 \
-    --gamma=0.99 --gae_lambda=0.95 \
     --no_partial_reset --reconfiguration_freq=1 \
     --exp-name="ppo-AnymalC-Reach-v1-state-${seed}-walltime_efficient" \
     --wandb_entity="stonet2000" --track
