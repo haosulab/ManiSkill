d3ab3cde4f273d44a9fa319debc353029b17c709
Stone Tao <stonezt2019@gmail.com>
2024-08-01 15:06:34 -0700

    work

--- examples/baselines/ppo/baselines.sh
+++ examples/baselines/ppo/baselines.sh
@@ -4,6 +4,8 @@
 # Furthermore, because of how these are evaluated, the hyperparameters here are tuned differently compared to with partial resets
 
 seeds=(9351 4796 1788)
+
+### State Based PPO Baselines ###
 for seed in ${seeds[@]}
 do
   python ppo.py --env_id="PushCube-v1" --seed=${seed} \
@@ -54,12 +56,12 @@ do
     --wandb_entity="stonet2000" --track
 done
 
-
+### RGB Based PPO Baselines ###
 for seed in ${seeds[@]}
 do
   python ppo_rgb.py --env_id="PickCube-v1" --seed=${seed} \
     --num_envs=256 --update_epochs=8 --num_minibatches=8 --reward_scale=1 \
-    --total_timesteps=50_000_000 --num_eval_envs=16 --target_kl=0.1 \
+    --total_timesteps=50_000_000 --num_eval_envs=16 \
     --no_partial_reset --reconfiguration_freq=1 \
-    --exp-name="ppo-PickCube-v1-rgb-${seed}-walltime_efficient2" 
+    --exp-name="ppo-PickCube-v1-rgb-${seed}-walltime_efficient" 
 done
