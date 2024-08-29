3ef489bd03ef42e92ec979b38185ebb2b26aba4c
Xander <xhinrichsen@ucsd.edu>
2024-08-29 00:00:33 -0700

    added rgb only obs examples

--- examples/baselines/ppo/examples.sh
+++ examples/baselines/ppo/examples.sh
@@ -113,6 +113,16 @@ python ppo_rgb.py --env_id="PickSingleYCB-v1" \
 python ppo_rgb.py --env_id="PushT-v1" \
   --num_envs=256 --update_epochs=8 --num_minibatches=8 \
   --total_timesteps=25_000_000 --num-steps=100 --num_eval_steps=100 --gamma=0.99
+python ppo_rgb.py --env_id="MS-AntWalk-v1" \
+ --num_envs=256 --update_epochs=8 --num_minibatches=32 \
+ --total_timesteps=5_000_000 --eval_freq=15 --num_eval_steps=1000 \
+ --num_steps=200 --gamma=0.97 --no-include-state --render_mode="rgb_array" \
+ --ent_coef=1e-3
+python ppo_rgb.py --env_id="MS-AntRun-v1" \
+ --num_envs=256 --update_epochs=8 --num_minibatches=32 \
+ --total_timesteps=15_000_000 --eval_freq=15 --num_eval_steps=1000 \
+ --num_steps=200 --gamma=0.97 --no-include-state --render_mode="rgb_array" \
+ --ent_coef=1e-3
 python ppo_rgb.py --env_id="MS-HumanoidRun-v1" \
   --num_envs=256 --update_epochs=8 --num_minibatches=32 \
   --total_timesteps=80_000_000 --eval_freq=15 --num_eval_steps=1000 \
