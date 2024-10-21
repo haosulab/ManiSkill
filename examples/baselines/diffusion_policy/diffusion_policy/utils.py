46c3b227650327df68b8b4034986efa576bb0148
Arnav G <73305591+arnavg115@users.noreply.github.com>
2024-10-21 19:58:27 +0000

    Removed unused code

--- examples/baselines/diffusion_policy/diffusion_policy/utils.py
+++ examples/baselines/diffusion_policy/diffusion_policy/utils.py
@@ -207,33 +207,3 @@ def build_state_obs_extractor(env_id):
     else:
         raise NotImplementedError(f"Please tune state obs by yourself")
 
-
-class RandomShiftsAug(nn.Module):
-    """
-    from https://github.com/facebookresearch/drqv2/blob/main/drqv2.py
-    """
-
-    def __init__(self, pad):
-        super().__init__()
-        self.pad = pad
-
-    def forward(self, x):
-        n, c, h, w = x.size()
-        assert h == w
-        padding = tuple([self.pad] * 4)
-        x = F.pad(x, padding, "replicate")
-        eps = 1.0 / (h + 2 * self.pad)
-        arange = torch.linspace(
-            -1.0 + eps, 1.0 - eps, h + 2 * self.pad, device=x.device, dtype=x.dtype
-        )[:h]
-        arange = arange.unsqueeze(0).repeat(h, 1).unsqueeze(2)
-        base_grid = torch.cat([arange, arange.transpose(1, 0)], dim=2)
-        base_grid = base_grid.unsqueeze(0).repeat(n, 1, 1, 1)
-
-        shift = torch.randint(
-            0, 2 * self.pad + 1, size=(n, 1, 1, 2), device=x.device, dtype=x.dtype
-        )
-        shift *= 2.0 / (h + 2 * self.pad)
-
-        grid = base_grid + shift
-        return F.grid_sample(x, grid, padding_mode="zeros", align_corners=False)
