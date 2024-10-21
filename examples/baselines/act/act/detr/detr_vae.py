4932d9bccb36309892ce3b30d6020f2f8e3d583a
ywchoi02 <83618525+ywchoi02@users.noreply.github.com>
2024-10-20 23:44:48 -0700

    change to absolute import

--- examples/baselines/act/act/detr/detr_vae.py
+++ examples/baselines/act/act/detr/detr_vae.py
@@ -5,7 +5,7 @@ DETR model and criterion classes.
 import torch
 from torch import nn
 from torch.autograd import Variable
-from .transformer import build_transformer, TransformerEncoder, TransformerEncoderLayer
+from examples.baselines.act.act.detr.transformer import build_transformer, TransformerEncoder, TransformerEncoderLayer
 
 import numpy as np
 
