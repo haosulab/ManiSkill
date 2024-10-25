683fa26f1cb0e2c3dfdf2538ce5fae2c7fa37f93
Viswesh-N <nvichu1001@gmail.com>
2024-10-25 14:02:35 -0700

    modified planner code to include new solution

--- mani_skill/examples/motionplanning/panda/solutions/__init__.py
+++ mani_skill/examples/motionplanning/panda/solutions/__init__.py
@@ -3,3 +3,4 @@ from .stack_cube import solve as solveStackCube
 from .peg_insertion_side import solve as solvePegInsertionSide
 from .plug_charger import solve as solvePlugCharger
 from .push_cube import solve as solvePushCube
+from .pull_cube_tool import solve as solvePullCubeTool
