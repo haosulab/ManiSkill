3abbbaefa71afe88913147e9e05908d39615e4eb
StoneT2000 <stonezt2019@gmail.com>
2024-10-29 16:56:06 -0700

    [BugFix] Fix ppo rgb reconfigure args and flatten wrapper

--- examples/baselines/ppo/ppo_rgb.py
+++ examples/baselines/ppo/ppo_rgb.py
@@ -294,8 +294,8 @@ if __name__ == "__main__":
 
     # env setup
     env_kwargs = dict(obs_mode="rgb", control_mode="pd_joint_delta_pos", render_mode=args.render_mode, sim_backend="gpu")
-    eval_envs = gym.make(args.env_id, num_envs=args.num_eval_envs, reconfigure_freq=args.eval_reconfiguration_freq, **env_kwargs)
-    envs = gym.make(args.env_id, num_envs=args.num_envs if not args.evaluate else 1, reconfigure_freq=args.reconfiguration_freq, **env_kwargs)
+    eval_envs = gym.make(args.env_id, num_envs=args.num_eval_envs, reconfiguration_freq=args.eval_reconfiguration_freq, **env_kwargs)
+    envs = gym.make(args.env_id, num_envs=args.num_envs if not args.evaluate else 1, reconfiguration_freq=args.reconfiguration_freq, **env_kwargs)
 
     # rgbd obs mode returns a dict of data, we flatten it so there is just a rgbd key and state key
     envs = FlattenRGBDObservationWrapper(envs, rgb=True, depth=False, state=args.include_state)
