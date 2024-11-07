b1cc15b214986663d8f641bafdddf7603c5c1c4b
Stone Tao <stonezt2019@gmail.com>
2024-11-07 12:28:52 -0800

    work

--- scripts/data_generation/rl.sh
+++ scripts/data_generation/rl.sh
@@ -4,6 +4,7 @@
 # to use these commands you need to install torchrl and tensordict. If cudagraphs does not work you can remove that flag
 # then go to the examples/baselines/ppo folder
 
+
 python ppo_fast.py --env_id="PickCube-v1" \
   --num_envs=4096 --num-steps=4 --update_epochs=8 --num_minibatches=32 \
   --total_timesteps=5_000_000 --eval_freq=100 \
@@ -30,3 +31,17 @@ python ppo_fast.py --env_id="PushT-v1" \
 python ppo_fast.py --env_id="PushT-v1" --evaluate \
   --checkpoint=runs/data_generation/PushT-v1-ppo/final_ckpt.pt \
   --num_eval_envs=1024 --num-eval-steps=100 --no-capture-video --save-trajectory
+
+
+python ppo_fast.py --env_id="AnymalC-Reach-v1" \
+  --num_envs=4096 --num-steps=16 --update_epochs=8 --num_minibatches=32 \
+  --total_timesteps=10_000_000 --num-eval-steps=200 \
+  --gamma=0.99 --gae_lambda=0.95 \
+  --save-model --cudagraphs --exp-name="data_generation/AnymalC-Reach-v1-ppo"
+
+
+
+python train.py --env-id StackCube-v1 \
+  --demo-path ../../../demos/StackCube-v1/rl/trajectory.state.pd_joint_delta_pos.cuda.h5 \
+  --control-mode "pd_joint_delta_pos" --sim-backend "gpu" --num-demos 100 --max_episode_steps 50 \
+  --total_iters 30000 --num_eval_envs 100
