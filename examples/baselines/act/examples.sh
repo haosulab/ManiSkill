7f5f636e71a1084d4a7ec30f3ba7e4b315a20dcf
ywchoi02 <83618525+ywchoi02@users.noreply.github.com>
2024-11-17 14:22:59 -0800

    Update examples.sh

--- examples/baselines/act/examples.sh
+++ examples/baselines/act/examples.sh
@@ -2,17 +2,25 @@
 
 # Learning from motion planning generated demonstrations
 
+# PickCube-v1
+python -m mani_skill.trajectory.replay_trajectory \
+  --traj-path ~/.maniskill/demos/PickCube-v1/motionplanning/trajectory.h5 \
+  --use-first-env-state -c pd_ee_delta_pos -o state \
+  --save-traj --num-procs 10 -b cpu
+
+python train.py --env-id PickCube-v1 \
+  --demo-path ~/.maniskill/demos/PickCube-v1/motionplanning/trajectory.state.pd_ee_delta_pos.cpu.h5 \
+  --control-mode "pd_ee_delta_pos" --sim-backend "cpu" --max_episode_steps 100 --total_iters 5000 --save_freq 5000 
+
 # PushCube-v1
 python -m mani_skill.trajectory.replay_trajectory \
   --traj-path ~/.maniskill/demos/PushCube-v1/motionplanning/trajectory.h5 \
   --use-first-env-state -c pd_ee_delta_pos -o state \
   --save-traj --num-procs 10 -b cpu
 
-# PickCube-v1
-python -m mani_skill.trajectory.replay_trajectory \
-  --traj-path ~/.maniskill/demos/PickCube-v1/motionplanning/trajectory.h5 \
-  --use-first-env-state -c pd_ee_delta_pos -o state \
-  --save-traj --num-procs 10 -b cpu
+python train.py --env-id PushCube-v1 \
+  --demo-path ~/.maniskill/demos/PushCube-v1/motionplanning/trajectory.state.pd_ee_delta_pos.cpu.h5 \
+  --control-mode "pd_ee_delta_pos" --sim-backend "cpu" --max_episode_steps 100 --total_iters 20000 --save_freq 20000 
 
 # StackCube-v1
 python -m mani_skill.trajectory.replay_trajectory \
@@ -20,8 +28,16 @@ python -m mani_skill.trajectory.replay_trajectory \
   --use-first-env-state -c pd_ee_delta_pos -o state \
   --save-traj --num-procs 10 -b cpu
 
+python train.py --env-id StackCube-v1 \
+  --demo-path ~/.maniskill/demos/StackCube-v1/motionplanning/trajectory.state.pd_ee_delta_pos.cpu.h5 \
+  --control-mode "pd_ee_delta_pos" --sim-backend "cpu" --max_episode_steps 200 --total_iters 30000 --save_freq 30000
+
 # PegInsertionSide-v1
 python -m mani_skill.trajectory.replay_trajectory \
   --traj-path ~/.maniskill/demos/PegInsertionSide-v1/motionplanning/trajectory.h5 \
   --use-first-env-state -c pd_ee_delta_pose -o state \
   --save-traj --num-procs 10 -b cpu
+
+python train.py --env-id PegInsertionSide-v1 \
+  --demo-path ~/.maniskill/demos/PegInsertionSide-v1/motionplanning/trajectory.state.pd_ee_delta_pose.cpu.h5 \
+  --control-mode "pd_ee_delta_pose" --sim-backend "cpu" --max_episode_steps 300 --total_iters 300000 --save_freq 300000
