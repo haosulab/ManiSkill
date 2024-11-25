2dc2a3639c585baca13807a597a5ee0d6875a44f
Viswesh-N <nvichu1001@gmail.com>
2024-11-25 03:21:20 -0800

    test run

--- mani_skill/envs/tasks/tabletop/pull_drawer.py
+++ mani_skill/envs/tasks/tabletop/pull_drawer.py
@@ -78,7 +78,7 @@ class PullDrawerEnv(BaseEnv):
 
     @property
     def _default_human_render_camera_configs(self):
-        pose = sapien_utils.look_at([-0.7, 0.7, 0.4], [0.0, 0.0, 0.0])
+        pose = sapien_utils.look_at([-0.8, 0.7, 0.7], [0.0, 0.0, 0.0])
         return [
             CameraConfig(
                 "render_camera",
