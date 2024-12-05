e37e74fe134dadbe590b84c25431ab1abe0a3cd2
Viswesh Nagaswamy Rajesh <72874174+Viswesh-N@users.noreply.github.com>
2024-12-05 08:29:01 +0530

    draft planner

--- mani_skill/examples/motionplanning/panda/solutions/pull_drawer.py
+++ mani_skill/examples/motionplanning/panda/solutions/pull_drawer.py
@@ -20,30 +20,38 @@ def solve(env: PullDrawerEnv, seed=None, debug=False, vis=False):
     )
     
     # Get drawer handle position
+    # planner.open_gripper()
+
     drawer_link = env.drawer.links_map['drawer']
+
+
     handle_offset = np.array([-env.inner_width/2 - env.handle_offset, 0, 0])
     handle_pose = drawer_link.pose * sapien.Pose(handle_offset)
+
     
     # Pre-grasp pose slightly behind handle
-    approach_offset = sapien.Pose([0.05, 0, 0])  # Offset behind handle
+    approach_offset = sapien.Pose(p = [-0.1, 0, 0])  # Offset behind handle
     pre_grasp_pose = handle_pose * approach_offset
-    
-    # Move to pre-grasp
+    # First 90-degree rotation around x-axis, then 90-degree rotation around z-axis
+    pre_grasp_pose.q = torch.tensor([0.5, 0.5, 0.5, 0.5])
+
     res = planner.move_to_pose_with_screw(pre_grasp_pose)
-    if res == -1:
-        return res
-        
-    # Move to grasp
-    grasp_pose = handle_pose
+    
+
+    move_in_offset = sapien.Pose(p = [0, 0, 0.13])
+    grasp_pose = pre_grasp_pose * move_in_offset
+
     res = planner.move_to_pose_with_screw(grasp_pose)
+
+
     if res == -1:
         return res
     
     # Close gripper
     planner.close_gripper()
-    
+
     # Pull drawer
-    pull_offset = sapien.Pose([-env.max_pull_distance * 0.8, 0, 0])
+    pull_offset = sapien.Pose([0, 0, -env.max_pull_distance * 0.81])
     pull_pose = grasp_pose * pull_offset
     res = planner.move_to_pose_with_screw(pull_pose)
     
@@ -56,6 +64,7 @@ def main():
         obs_mode="none",
         control_mode="pd_joint_pos",
         render_mode="rgb_array"
+        sim
     )
     
     for seed in range(100):
