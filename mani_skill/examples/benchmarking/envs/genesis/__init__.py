037943adb4597241ae8d7bfe017039d020da31c1
StoneT2000 <stonezt2019@gmail.com>
2024-12-20 18:43:49 -0800

    [BugFix] remove missing import in benchmark

--- mani_skill/examples/benchmarking/envs/genesis/__init__.py
+++ mani_skill/examples/benchmarking/envs/genesis/__init__.py
@@ -2,7 +2,6 @@ import gymnasium as gym
 
 from .franka_move import FrankaMoveBenchmarkEnv
 from .franka_pick_cube import FrankaPickCubeBenchmarkEnv
-from .franka_stack_cube import FrankaStackCubeBenchmarkEnv
 gym.register(
     id="FrankaMoveBenchmark-v1",
     entry_point="envs.genesis.franka_move:FrankaMoveBenchmarkEnv",
