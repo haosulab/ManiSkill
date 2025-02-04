49c988ec4614716e49f4c0f4afd652b43fdebd5b
AlexandreBrown <alexandrebrown97@gmail.com>
2025-02-04 13:52:05 -0500

    Fixed final_info having wrong elapsed_steps value after reset - Because BaseEnv::get_info does not always clone the elapsed_steps, the dict still pointed to the same tensor therefore the env.reset would change the elapsed_steps stored which would invalidate the final_info data - My change is local to the vect env to stay safe

--- mani_skill/vector/wrappers/gymnasium.py
+++ mani_skill/vector/wrappers/gymnasium.py
@@ -4,6 +4,7 @@ from typing import TYPE_CHECKING, Dict, List, Optional, Tuple, Union
 
 import gymnasium as gym
 import torch
+import copy
 from gymnasium.vector import VectorEnv
 
 from mani_skill.utils.structs.types import Array
@@ -143,8 +144,8 @@ class ManiSkillVectorEnv(VectorEnv):
         if dones.any() and self.auto_reset:
             final_obs = obs
             env_idx = torch.arange(0, self.num_envs, device=self.device)[dones]
+            infos["final_info"] = copy.deepcopy(infos)
             obs, _ = self.reset(options=dict(env_idx=env_idx))
-            infos["final_info"] = infos.copy()
             # gymnasium calls it final observation but it really is just o_{t+1} or the true next observation
             infos["final_observation"] = final_obs
             # NOTE (stao): that adding masks like below is a bit redundant and not necessary
