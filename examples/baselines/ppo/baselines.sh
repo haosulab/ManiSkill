7412b4928543b608980d402a178c22912bae74a5
Stone Tao <stonezt2019@gmail.com>
2025-02-20 13:20:43 -0800

    save models by default in ppo fast, default was false before when other code was true

--- examples/baselines/ppo/baselines.sh
+++ examples/baselines/ppo/baselines.sh
@@ -9,7 +9,7 @@ do
     --num_envs=4096 --num-steps=4 --update_epochs=8 --num_minibatches=32 \
     --total_timesteps=50_000_000 \
     --num_eval_envs=16 \
-    --save-model --cudagraphs --exp-name="ppo-PushCube-v1-state-${seed}-walltime_efficient" \
+    --cudagraphs --exp-name="ppo-PushCube-v1-state-${seed}-walltime_efficient" \
     --track
 done
 
@@ -19,7 +19,7 @@ do
     --num_envs=4096 --num-steps=4 --update_epochs=8 --num_minibatches=32 \
     --total_timesteps=50_000_000 \
     --num_eval_envs=16 \
-    --save-model --cudagraphs --exp-name="ppo-PickCube-v1-state-${seed}-walltime_efficient" \
+    --cudagraphs --exp-name="ppo-PickCube-v1-state-${seed}-walltime_efficient" \
     --track
 done
 
@@ -29,7 +29,7 @@ do
     --num_envs=4096 --num-steps=16 --update_epochs=8 --num_minibatches=32 --gamma=0.99 \
     --total_timesteps=50_000_000 --num_eval_steps=100 \
     --num_eval_envs=16 \
-    --save-model --cudagraphs --exp-name="ppo-PushT-v1-state-${seed}-walltime_efficient" \
+    --cudagraphs --exp-name="ppo-PushT-v1-state-${seed}-walltime_efficient" \
     --track
 done
 
@@ -39,7 +39,7 @@ do
     --num_envs=4096 --num-steps=16 --update_epochs=8 --num_minibatches=32 \
     --total_timesteps=50_000_000 \
     --num_eval_envs=16 \
-    --save-model --cudagraphs --exp-name="ppo-StackCube-v1-state-${seed}-walltime_efficient" \
+    --cudagraphs --exp-name="ppo-StackCube-v1-state-${seed}-walltime_efficient" \
     --track
 done
 
@@ -49,7 +49,7 @@ do
     --num_envs=4096 --num-steps=16 --update_epochs=8 --num_minibatches=32 --gamma=0.95 \
     --total_timesteps=50_000_000 --num-eval-steps=80 \
     --num_eval_envs=16 \
-    --save-model --cudagraphs --exp-name="ppo-RollBall-v1-state-${seed}-walltime_efficient" \
+    --cudagraphs --exp-name="ppo-RollBall-v1-state-${seed}-walltime_efficient" \
     --track
 done
 
@@ -59,7 +59,7 @@ do
     --num_envs=4096 --num-steps=4 --update_epochs=8 --num_minibatches=32 \
     --total_timesteps=50_000_000 \
     --num_eval_envs=16 \
-    --save-model --cudagraphs --exp-name="ppo-PullCube-v1-state-${seed}-walltime_efficient" \
+    --cudagraphs --exp-name="ppo-PullCube-v1-state-${seed}-walltime_efficient" \
     --track
 done
 
@@ -69,7 +69,7 @@ do
     --num_envs=4096 --num-steps=4 --update_epochs=8 --num_minibatches=32 \
     --total_timesteps=50_000_000 \
     --num_eval_envs=16 \
-    --save-model --cudagraphs --exp-name="ppo-PokeCube-v1-state-${seed}-walltime_efficient" \
+    --cudagraphs --exp-name="ppo-PokeCube-v1-state-${seed}-walltime_efficient" \
     --track
 done
 
@@ -79,7 +79,7 @@ do
     --num_envs=4096 --num-steps=4 --update_epochs=8 --num_minibatches=32 \
     --total_timesteps=50_000_000 \
     --num_eval_envs=16 \
-    --save-model --cudagraphs --exp-name="ppo-LiftPegUpright-v1-state-${seed}-walltime_efficient" \
+    --cudagraphs --exp-name="ppo-LiftPegUpright-v1-state-${seed}-walltime_efficient" \
     --track
 done
 
@@ -89,7 +89,7 @@ do
     --num_envs=4096 --update_epochs=8 --num_minibatches=32 --gamma=0.99 --gae_lambda=0.95 \
     --total_timesteps=50_000_000 --num-steps=16 --num-eval-steps=200 \
     --num_eval_envs=16 \
-    --save-model --cudagraphs --exp-name="ppo-AnymalC-Reach-v1-state-${seed}-walltime_efficient" \
+    --cudagraphs --exp-name="ppo-AnymalC-Reach-v1-state-${seed}-walltime_efficient" \
     --track
 done
 
@@ -99,7 +99,7 @@ do
     --num_envs=2048 --update_epochs=8 --num_minibatches=32 --gamma=0.97 --gae_lambda=0.95 \
     --total_timesteps=75_000_000 --num-steps=16 --num-eval-steps=100 \
     --num_eval_envs=16 \
-    --save-model --cudagraphs --exp-name="ppo-PegInsertionSide-v1-state-${seed}-walltime_efficient" \
+    --cudagraphs --exp-name="ppo-PegInsertionSide-v1-state-${seed}-walltime_efficient" \
     --track
 done
 
@@ -109,7 +109,7 @@ do
     --num_envs=1024 --update_epochs=8 --num_minibatches=32 \
     --total_timesteps=50_000_000 --num-steps=100 --num-eval-steps=100 \
     --num_eval_envs=16 \
-    --save-model --cudagraphs --exp-name="ppo-TwoRobotPickCube-v1-state-${seed}-walltime_efficient" \
+    --cudagraphs --exp-name="ppo-TwoRobotPickCube-v1-state-${seed}-walltime_efficient" \
     --track
 done
 
@@ -119,7 +119,7 @@ do
     --num_envs=1024 --update_epochs=8 --num_minibatches=32 \
     --total_timesteps=50_000_000 --num-steps=32 --num-eval-steps=100 \
     --num_eval_envs=16 \
-    --save-model --cudagraphs --exp-name="ppo-UnitreeG1PlaceAppleInBowl-v1-state-${seed}-walltime_efficient" \
+    --cudagraphs --exp-name="ppo-UnitreeG1PlaceAppleInBowl-v1-state-${seed}-walltime_efficient" \
     --track
 done
 
@@ -129,7 +129,7 @@ do
     --num_envs=1024 --update_epochs=8 --num_minibatches=32 \
     --total_timesteps=100_000_000 --num-steps=32 --num-eval-steps=100 \
     --num_eval_envs=16 \
-    --save-model --cudagraphs --exp-name="ppo-UnitreeG1TransportBox-v1-state-${seed}-walltime_efficient" \
+    --cudagraphs --exp-name="ppo-UnitreeG1TransportBox-v1-state-${seed}-walltime_efficient" \
     --track
 done
 
@@ -139,7 +139,7 @@ do
     --num_envs=1024 --update_epochs=8 --num_minibatches=32 \
     --total_timesteps=50_000_000 --num-steps=16 --num-eval-steps=100 \
     --num_eval_envs=16 \
-    --save-model --cudagraphs --exp-name="ppo-OpenCabinetDrawer-v1-state-${seed}-walltime_efficient" \
+    --cudagraphs --exp-name="ppo-OpenCabinetDrawer-v1-state-${seed}-walltime_efficient" \
     --track
 done
 
@@ -149,7 +149,7 @@ do
     --num_envs=4096 --num-steps=16 --update_epochs=8 --num_minibatches=32 \
     --total_timesteps=50_000_000 \
     --num_eval_envs=16 \
-    --save-model --cudagraphs --exp-name="ppo-PickSingleYCB-v1-state-${seed}-walltime_efficient" \
+    --cudagraphs --exp-name="ppo-PickSingleYCB-v1-state-${seed}-walltime_efficient" \
     --track
 done
 
@@ -191,5 +191,15 @@ do
     --total_timesteps=50_000_000 --num-steps=16 --num-eval-steps=200 \
     --num_eval_envs=16 --eval-reconfiguration-freq=0 \
     --exp-name="ppo-AnymalC-Reach-v1-rgb-${seed}-walltime_efficient" \
+    --track
+done
+
+for seed in ${seeds[@]}
+do
+  python ppo_rgb.py --env_id="PickSingleYCB-v1" --seed=${seed} \
+    --num_envs=1024 --num-steps=16 --update_epochs=8 --num_minibatches=32 \
+    --total_timesteps=50_000_000 \
+    --num_eval_envs=16 \
+    --exp-name="ppo-PickSingleYCB-v1-rgb-${seed}-walltime_efficient" \
     --track
 done
