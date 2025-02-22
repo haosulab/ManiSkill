5fb50fdeca88f23f8315cc32200d3d67247802b8
StoneT2000 <stonezt2019@gmail.com>
2025-02-21 21:55:30 -0800

    [BugFix] wrong name for cuda render backend check

--- mani_skill/utils/structs/render_camera.py
+++ mani_skill/utils/structs/render_camera.py
@@ -168,7 +168,7 @@ class RenderCamera:
             elif SAPIEN_RENDER_SYSTEM == "3.1":
                 return [x.torch() for x in self.camera_group.get_cuda_pictures(names)]
         else:
-            if self.scene.backend.render_backend == "cuda":
+            if self.scene.backend.render_backend == "sapien_cuda":
                 return [
                     self._render_cameras[0].get_picture_cuda(name).torch()[None, ...]
                     for name in names
