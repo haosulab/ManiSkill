bc0cdf1513cc6e6c9e23124977c07a1a3ed6c846
Jeremy Morgan <jmorgan@theaiinstitute.com>
2025-02-25 20:23:17 -0800

    added pick_cube_v2.py

--- mani_skill/examples/motionplanning/panda/run.py
+++ mani_skill/examples/motionplanning/panda/run.py
@@ -13,6 +13,7 @@ from mani_skill.examples.motionplanning.panda.solutions import solvePushCube, so
 MP_SOLUTIONS = {
     "DrawTriangle-v1": solveDrawTriangle,
     "PickCube-v1": solvePickCube,
+    "PickCube-v2": solvePickCube,
     "StackCube-v1": solveStackCube,
     "PegInsertionSide-v1": solvePegInsertionSide,
     "PlugCharger-v1": solvePlugCharger,
@@ -39,6 +40,8 @@ def parse_args(args=None):
     parser.add_argument("--shader", default="default", type=str, help="Change shader used for rendering. Default is 'default' which is very fast. Can also be 'rt' for ray tracing and generating photo-realistic renders. Can also be 'rt-fast' for a faster but lower quality ray-traced renderer")
     parser.add_argument("--record-dir", type=str, default="demos", help="where to save the recorded trajectories")
     parser.add_argument("--num-procs", type=int, default=1, help="Number of processes to use to help parallelize the trajectory replay process. This uses CPU multiprocessing and only works with the CPU simulation backend at the moment.")
+    parser.add_argument("--camera-width", type=int, default=128, help="Width of the camera.")
+    parser.add_argument("--camera-height", type=int, default=128, help="Height of the camera.")
     return parser.parse_args()
 
 def _main(args, proc_id: int = 0, start_seed: int = 0) -> str:
@@ -51,7 +54,9 @@ def _main(args, proc_id: int = 0, start_seed: int = 0) -> str:
         sensor_configs=dict(shader_pack=args.shader),
         human_render_camera_configs=dict(shader_pack=args.shader),
         viewer_camera_configs=dict(shader_pack=args.shader),
-        sim_backend=args.sim_backend
+        sim_backend=args.sim_backend,
+        camera_width=args.camera_width,
+        camera_height=args.camera_height
     )
     if env_id not in MP_SOLUTIONS:
         raise RuntimeError(f"No already written motion planning solutions for {env_id}. Available options are {list(MP_SOLUTIONS.keys())}")
