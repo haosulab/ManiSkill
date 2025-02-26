7f09d903747323fb14f9f114bbf61a8c9c257b79
Viswesh N <72874174+Viswesh-N@users.noreply.github.com>
2025-02-26 13:30:49 -0800

    [Feature] PullDrawer-v1 Env with reward function and motion planner (#774)
    * first draft of drawer env
    
    * fixed spawn and dimensions, need to debug joint
    
    * drawer construction bug fix
    
    * drawer construction bug fix
    
    * drawer construction bug fix
    
    * drawer construction bug fix
    
    * mostly fixed drawer, need to verify dense reward
    
    * main axis motion fix
    
    * reward function experiment
    
    * test run
    
    * test run
    
    * test run
    
    * fixed handle bugs
    
    * Update pull_drawer.py
    
    * added draft planner, needs env fix
    
    * Update pull_drawer.py
    
    * Update pull_drawer.py
    
    * Update pull_drawer.py
    
    * Update pull_drawer.py
    
    * Update pull_drawer.py
    
    * modified offset for grasp
    
    * modified offset for grasp
    
    * test sphere for goal
    
    * fixed handle pose handling issue
    
    * tests for run
    
    * Update pull_drawer.py
    
    * Update pull_drawer.py
    
    * expt new reward
    
    * modified reward expt
    
    * Update pull_drawer.py
    
    * Update pull_drawer.py
    
    * Update pull_drawer.py
    
    * Update pull_drawer.py
    
    * Update pull_drawer.py
    
    * Update pull_drawer.py
    
    * reward forcing expt
    
    * Update pull_drawer.py
    
    * planner test
    
    * planner test
    
    * Update pull_drawer.py
    
    * Update pull_drawer.py
    
    * Update pull_drawer.py
    
    * draft planner
    
    * Updated env with spawn randomness
    
    * Update pull_drawer.py
    
    * revert to last working reward version
    
    * orientation expt
    
    * Update pull_drawer.py
    
    * Update pull_drawer.py
    
    * Update pull_drawer.py
    
    * final working planner and dense reward
    
    * Update pull_drawer.py
    
    * fully working planner with seeding
    
    * improved dense reward
    
    * update motion planning sh with new env
    
    * added sample ppo script
    
    * added docs for new task
    
    * final cleanup, working dense reward
    
    * increase randomness
    
    * docker configs
    
    ---------
    
    Co-authored-by: Viswesh Nagaswamy Rajesh <visweshn2003@Visweshs-MacBook-Air.local>
    Co-authored-by: Stone Tao <stonezt2019@gmail.com>

--- mani_skill/examples/motionplanning/panda/run.py
+++ mani_skill/examples/motionplanning/panda/run.py
@@ -9,7 +9,7 @@ from tqdm import tqdm
 import os.path as osp
 from mani_skill.utils.wrappers.record import RecordEpisode
 from mani_skill.trajectory.merge_trajectory import merge_trajectories
-from mani_skill.examples.motionplanning.panda.solutions import solvePushCube, solvePickCube, solveStackCube, solvePegInsertionSide, solvePlugCharger, solvePullCubeTool, solveLiftPegUpright, solvePullCube, solveDrawTriangle
+from mani_skill.examples.motionplanning.panda.solutions import solvePushCube, solvePickCube, solveStackCube, solvePegInsertionSide, solvePlugCharger, solvePullCubeTool, solveLiftPegUpright, solvePullCube, solveDrawTriangle, pull-drawer-env
 MP_SOLUTIONS = {
     "DrawTriangle-v1": solveDrawTriangle,
     "PickCube-v1": solvePickCube,
@@ -19,7 +19,8 @@ MP_SOLUTIONS = {
     "PushCube-v1": solvePushCube,
     "PullCubeTool-v1": solvePullCubeTool,
     "LiftPegUpright-v1": solveLiftPegUpright,
-    "PullCube-v1": solvePullCube
+    "PullCube-v1": solvePullCube,
+    "PullDrawer-v1": solvePullDrawer
 
 }
 def parse_args(args=None):
