f5c5d202fa36995e5135bd8973c72837bbc0fba5
Jeremy Morgan <jmorgan@theaiinstitute.com>
2025-02-28 17:50:20 -0800

    added plugChargerV2, PushCubeV2

---
+++ mani_skill/envs/tasks/tabletop/push_cube_v2.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+from mani_skill.envs.tasks.tabletop.push_cube import PushCubeEnv
+from mani_skill.sensors.camera import CameraConfig
+from mani_skill.utils import sapien_utils
+from mani_skill.utils.registration import register_env
+
+DEFAULT_CAMERA_WIDTH = 128
+DEFAULT_CAMERA_HEIGHT = 128
+
+@register_env("PushCube-v2", max_episode_steps=50)
+class PushCubeV2Env(PushCubeEnv):
+    """
+    **Task Description:**
+    Nearly exacty copy of PushCubeEnv, but with 3 cameras instead of 1.
+    """
+    def __init__(self, *args, robot_uids="panda", robot_init_qpos_noise=0.02, **kwargs):
+        self._camera_width = kwargs.pop("camera_width", DEFAULT_CAMERA_WIDTH)
+        self._camera_height = kwargs.pop("camera_height", DEFAULT_CAMERA_HEIGHT)
+        super().__init__(*args, robot_uids=robot_uids, robot_init_qpos_noise=robot_init_qpos_noise, **kwargs)
+
+    @property
+    def _default_sensor_configs(self):
+        pose_center = sapien_utils.look_at(eye=[0.3, 0, 0.6], target=[-0.1, 0, 0.1])
+        pose_left = sapien_utils.look_at(eye=[0.0, -0.3, 0.6], target=[-0.1, 0, 0.1])
+        pose_right = sapien_utils.look_at(eye=[0.0, 0.3, 0.6], target=[-0.1, 0, 0.1])
+        return [
+            CameraConfig("camera_center", pose_center, self._camera_width, self._camera_height, np.pi / 2, 0.01, 100),
+            CameraConfig("camera_left", pose_left, self._camera_width, self._camera_height, np.pi / 2, 0.01, 100),
+            CameraConfig("camera_right", pose_right, self._camera_width, self._camera_height, np.pi / 2, 0.01, 100),
+        ]
