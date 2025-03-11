3679db8507d7508eda12b396da90d8cdb9842fd3
hu.po <hugo.p.cmu@gmail.com>
2025-03-11 15:57:45 -0500

    remove examples, add baselines

--- examples/baselines/ppo/baselines.sh
+++ examples/baselines/ppo/baselines.sh
@@ -153,6 +153,16 @@ do
     --track
 done
 
+for seed in ${seeds[@]}
+do
+  python ppo_fast.py --env_id="PickCube-v1" --robot_uids="widowxai" --seed=${seed} \
+    --num_envs=4096 --num-steps=4 --update_epochs=8 --num_minibatches=32 \
+    --total_timesteps=50_000_000 \
+    --num_eval_envs=16 \
+    --cudagraphs --exp-name="ppo-PickCube-v1-state-${seed}-walltime_efficient" \
+    --track
+done
+
 ### RGB Based PPO Baselines ###
 for seed in ${seeds[@]}
 do
@@ -201,5 +211,15 @@ do
     --total_timesteps=50_000_000 \
     --num_eval_envs=16 \
     --exp-name="ppo-PickSingleYCB-v1-rgb-${seed}-walltime_efficient" \
+    --track
+done
+
+for seed in ${seeds[@]}
+do
+  python ppo_rgb.py --env_id="PickCube-v1" --robot_uids="widowxai_cam" --seed=${seed} \
+    --num_envs=1024 --num-steps=16 --update_epochs=8 --num_minibatches=32 \
+    --total_timesteps=50_000_000 \
+    --num_eval_envs=16 \
+    --exp-name="ppo-PickCube-v1-rgb-${seed}-walltime_efficient" \
     --track
 done
