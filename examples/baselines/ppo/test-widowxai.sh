f408544d88d05d9d64903780b0b16d1ef3d58e8c
hu.po <hugo.p.cmu@gmail.com>
2025-03-12 09:04:28 -0500

    tune

--- examples/baselines/ppo/test-widowxai.sh
+++ examples/baselines/ppo/test-widowxai.sh
@@ -1,21 +1,27 @@
 seeds=(9351 4796 1788)
+steps=(50 100 150)
 
-for seed in ${seeds[@]}
+for step in ${steps[@]}
 do
-  python ppo_fast.py --env_id="PickCube-v1" --robot_uids="widowxai" --seed=${seed} \
-    --num_envs=4096 --num-steps=4 --update_epochs=8 --num_minibatches=32 \
-    --total_timesteps=50_000_000 \
-    --num_eval_envs=16 \
-    --cudagraphs --exp-name="ppo-PickCube-v1-state-${seed}-walltime_efficient" \
-    --track
-done
+  for seed in ${seeds[@]}
+  do
+    python ppo_fast.py --env_id="PickCube-v1" --robot_uids="widowxai" --seed=${seed} \
+      --num_envs=4096 --update_epochs=8 --num_minibatches=32 \
+      --num_steps=${step} --num_eval_steps=${step} \
+      --total_timesteps=100_000_000 \
+      --num_eval_envs=16 \
+      --compile --exp-name="ppo-PickCube-v1-state-${seed}-step${step}" \
+      --track
+  done
 
-for seed in ${seeds[@]}
-do
-  python ppo_rgb.py --env_id="PickCube-v1" --robot_uids="widowxai_cam" --seed=${seed} \
-    --num_envs=1024 --num-steps=16 --update_epochs=8 --num_minibatches=32 \
-    --total_timesteps=50_000_000 \
-    --num_eval_envs=16 \
-    --exp-name="ppo-PickCube-v1-rgb-${seed}-walltime_efficient" \
-    --track
+  for seed in ${seeds[@]}
+  do
+    python ppo_rgb.py --env_id="PickCube-v1" --robot_uids="widowxai_cam" --seed=${seed} \
+      --num_envs=1024 --update_epochs=8 --num_minibatches=32 \
+      --num_steps=${step} --num_eval_steps=${step} \
+      --total_timesteps=100_000_000 \
+      --num_eval_envs=16 \
+      --exp-name="ppo-PickCube-v1-rgb-${seed}-step${step}" \
+      --track
+  done
 done
