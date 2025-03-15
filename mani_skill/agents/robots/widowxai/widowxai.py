d406e465a3c05aade072de53592b8778dec20f15
hu.po <hugo.p.cmu@gmail.com>
2025-03-15 13:06:41 -0500

    removed control modes

--- mani_skill/agents/robots/widowxai/widowxai.py
+++ mani_skill/agents/robots/widowxai/widowxai.py
@@ -62,10 +62,10 @@ class WidowXAI(BaseAgent):
     ]
     ee_link_name = "ee_gripper_link"
 
+    # NOTE(hupo): these are mostly copied from Panda, need to be tuned for real robot
     arm_stiffness = 1e3
     arm_damping = 1e2
     arm_force_limit = 100
-
     gripper_stiffness = 1e3
     gripper_damping = 1e2
     gripper_force_limit = 100
@@ -73,17 +73,8 @@ class WidowXAI(BaseAgent):
     @property
     def _controller_configs(self):
         # -------------------------------------------------------------------------- #
-        # Arm
+        # Arm 
         # -------------------------------------------------------------------------- #
-        arm_pd_joint_pos = PDJointPosControllerConfig(
-            self.arm_joint_names,
-            lower=None,
-            upper=None,
-            stiffness=self.arm_stiffness,
-            damping=self.arm_damping,
-            force_limit=self.arm_force_limit,
-            normalize_action=False,
-        )
         arm_pd_joint_delta_pos = PDJointPosControllerConfig(
             self.arm_joint_names,
             lower=-0.1,
@@ -118,18 +109,6 @@ class WidowXAI(BaseAgent):
             force_limit=self.arm_force_limit,
             ee_link=self.ee_link_name,
             urdf_path=self.urdf_path,
-        )
-        arm_pd_ee_pose = PDEEPoseControllerConfig(
-            joint_names=self.arm_joint_names,
-            pos_lower=None,
-            pos_upper=None,
-            stiffness=self.arm_stiffness,
-            damping=self.arm_damping,
-            force_limit=self.arm_force_limit,
-            ee_link=self.ee_link_name,
-            urdf_path=self.urdf_path,
-            use_delta=False,
-            normalize_action=False,
         )
 
         arm_pd_ee_target_delta_pos = deepcopy(arm_pd_ee_delta_pos)
@@ -181,33 +160,15 @@ class WidowXAI(BaseAgent):
         )
 
         controller_configs = dict(
-            pd_joint_delta_pos=dict(
-                arm=arm_pd_joint_delta_pos, gripper=gripper_pd_joint_pos
-            ),
-            pd_joint_pos=dict(arm=arm_pd_joint_pos, gripper=gripper_pd_joint_pos),
+            pd_joint_delta_pos=dict(arm=arm_pd_joint_delta_pos, gripper=gripper_pd_joint_pos),
             pd_ee_delta_pos=dict(arm=arm_pd_ee_delta_pos, gripper=gripper_pd_joint_pos),
-            pd_ee_delta_pose=dict(
-                arm=arm_pd_ee_delta_pose, gripper=gripper_pd_joint_pos
-            ),
-            pd_ee_pose=dict(arm=arm_pd_ee_pose, gripper=gripper_pd_joint_pos),
-            # TODO(jigu): how to add boundaries for the following controllers
-            pd_joint_target_delta_pos=dict(
-                arm=arm_pd_joint_target_delta_pos, gripper=gripper_pd_joint_pos
-            ),
-            pd_ee_target_delta_pos=dict(
-                arm=arm_pd_ee_target_delta_pos, gripper=gripper_pd_joint_pos
-            ),
-            pd_ee_target_delta_pose=dict(
-                arm=arm_pd_ee_target_delta_pose, gripper=gripper_pd_joint_pos
-            ),
-            # Caution to use the following controllers
+            pd_ee_delta_pose=dict(arm=arm_pd_ee_delta_pose, gripper=gripper_pd_joint_pos),
+            pd_joint_target_delta_pos=dict(arm=arm_pd_joint_target_delta_pos, gripper=gripper_pd_joint_pos),
+            pd_ee_target_delta_pos=dict(arm=arm_pd_ee_target_delta_pos, gripper=gripper_pd_joint_pos),
+            pd_ee_target_delta_pose=dict(arm=arm_pd_ee_target_delta_pose, gripper=gripper_pd_joint_pos),
             pd_joint_vel=dict(arm=arm_pd_joint_vel, gripper=gripper_pd_joint_pos),
-            pd_joint_pos_vel=dict(
-                arm=arm_pd_joint_pos_vel, gripper=gripper_pd_joint_pos
-            ),
-            pd_joint_delta_pos_vel=dict(
-                arm=arm_pd_joint_delta_pos_vel, gripper=gripper_pd_joint_pos
-            ),
+            pd_joint_pos_vel=dict(arm=arm_pd_joint_pos_vel, gripper=gripper_pd_joint_pos),
+            pd_joint_delta_pos_vel=dict(arm=arm_pd_joint_delta_pos_vel, gripper=gripper_pd_joint_pos),
         )
 
         # Make a deepcopy in case users modify any config
@@ -226,34 +187,17 @@ class WidowXAI(BaseAgent):
             min_force (float, optional): Minimum force before the robot is considered to be grasping the object in Newtons. Defaults to 0.5.
             max_angle (int, optional): Maximum angle of contact to consider grasping. Defaults to 85.
         """
-        l_contact_forces = self.scene.get_pairwise_contact_forces(
-            self.finger1_link, object
-        )
-        r_contact_forces = self.scene.get_pairwise_contact_forces(
-            self.finger2_link, object
-        )
+        l_contact_forces = self.scene.get_pairwise_contact_forces(self.finger1_link, object)
+        r_contact_forces = self.scene.get_pairwise_contact_forces(self.finger2_link, object)
         lforce = torch.linalg.norm(l_contact_forces, axis=1)
         rforce = torch.linalg.norm(r_contact_forces, axis=1)
-
-        # direction to open the gripper
         ldirection = self.finger1_link.pose.to_transformation_matrix()[..., :3, 1]
-        # print(f"ldirection: {ldirection}")
         rdirection = -self.finger2_link.pose.to_transformation_matrix()[..., :3, 1]
-        # print(f"rdirection: {rdirection}")
         langle = common.compute_angle_between(ldirection, l_contact_forces)
-        # print(f"langle: {langle}")
         rangle = common.compute_angle_between(rdirection, r_contact_forces)
-        # print(f"rangle: {rangle}")
-        lflag = torch.logical_and(
-            lforce >= min_force, torch.rad2deg(langle) <= max_angle
-        )
-        # print(f"lflag: {lflag}")
-        rflag = torch.logical_and(
-            rforce >= min_force, torch.rad2deg(rangle) <= max_angle
-        )
-        # print(f"rflag: {rflag}")
+        lflag = torch.logical_and(lforce >= min_force, torch.rad2deg(langle) <= max_angle)
+        rflag = torch.logical_and(rforce >= min_force, torch.rad2deg(rangle) <= max_angle)
         _is_grasped = torch.logical_and(lflag, rflag)
-        # print(f"is_grasped: {_is_grasped}")
         return _is_grasped
 
     def is_static(self, threshold: float = 0.2):
