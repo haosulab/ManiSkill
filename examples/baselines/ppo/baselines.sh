dcec7367c64509928c56abd5e5796c78644b9d30
hu.po <hugo.p.cmu@gmail.com>
2025-03-15 13:20:39 -0500

    cleanup

--- examples/baselines/ppo/baselines.sh
+++ examples/baselines/ppo/baselines.sh
@@ -155,11 +155,11 @@ done
 
 for seed in ${seeds[@]}
 do
-  python ppo_fast.py --env_id="PickCube-v1" --robot_uids="widowxai" --seed=${seed} \
-    --num_envs=4096 --num-steps=4 --update_epochs=8 --num_minibatches=32 \
+  python ppo_fast.py --env_id="PushCube-v1" --robot_uids="widowxai" --seed=${seed} \
+    --num_envs=4096 --update_epochs=8 --num_minibatches=32 \
     --total_timesteps=50_000_000 \
     --num_eval_envs=16 \
-    --cudagraphs --exp-name="ppo-PickCube-v1-state-${seed}-walltime_efficient" \
+    --cudagraphs --exp-name="ppo-PushCube-v1-state-widowxai-${seed}-walltime_efficient" \
     --track
 done
 
@@ -216,10 +216,10 @@ done
 
 for seed in ${seeds[@]}
 do
-  python ppo_rgb.py --env_id="PickCube-v1" --robot_uids="widowxai_cam" --seed=${seed} \
-    --num_envs=1024 --num-steps=16 --update_epochs=8 --num_minibatches=32 \
+  python ppo_rgb.py --env_id="PushCube-v1" --robot_uids="widowxai_cam" --seed=${seed} \
+    --num_envs=1024 --update_epochs=8 --num_minibatches=32 \
     --total_timesteps=50_000_000 \
     --num_eval_envs=16 \
-    --exp-name="ppo-PickCube-v1-rgb-${seed}-walltime_efficient" \
+    --exp-name="ppo-PushCube-v1-rgb-widowxai-${seed}-walltime_efficient" \
     --track
 done
