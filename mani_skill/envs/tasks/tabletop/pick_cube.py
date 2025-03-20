714baad572590900e86096bd64011ca1e0dc8bb1
Jeremy Morgan <jmorgan@theaiinstitute.com>
2025-03-20 16:20:27 -0700

    further changes to pick_cube_v2.py

--- mani_skill/envs/tasks/tabletop/pick_cube.py
+++ mani_skill/envs/tasks/tabletop/pick_cube.py
@@ -1,13 +1,12 @@
-from typing import Any, Union
+from typing import Any, Dict, Union
 
 import numpy as np
 import sapien
 import torch
 
 import mani_skill.envs.utils.randomization as randomization
-from mani_skill.agents.robots import SO100, Fetch, Panda, WidowXAI, XArm6Robotiq
+from mani_skill.agents.robots import Fetch, Panda, XArm6Robotiq
 from mani_skill.envs.sapien_env import BaseEnv
-from mani_skill.envs.tasks.tabletop.pick_cube_cfgs import PICK_CUBE_CONFIGS
 from mani_skill.sensors.camera import CameraConfig
 from mani_skill.utils import sapien_utils
 from mani_skill.utils.building import actors
@@ -15,66 +14,56 @@ from mani_skill.utils.registration import register_env
 from mani_skill.utils.scene_builder.table import TableSceneBuilder
 from mani_skill.utils.structs.pose import Pose
 
-PICK_CUBE_DOC_STRING = """**Task Description:**
-A simple task where the objective is to grasp a red cube with the {robot_id} robot and move it to a target goal position. This is also the *baseline* task to test whether a robot with manipulation
-capabilities can be simulated and trained properly. Hence there is extra code for some robots to set them up properly in this environment as well as the table scene builder.
-
-**Randomizations:**
-- the cube's xy position is randomized on top of a table in the region [0.1, 0.1] x [-0.1, -0.1]. It is placed flat on the table
-- the cube's z-axis rotation is randomized to a random angle
-- the target goal position (marked by a green sphere) of the cube has its xy position randomized in the region [0.1, 0.1] x [-0.1, -0.1] and z randomized in [0, 0.3]
-
-**Success Conditions:**
-- the cube position is within `goal_thresh` (default 0.025m) euclidean distance of the goal position
-- the robot is static (q velocity < 0.2)
-"""
-
 
 @register_env("PickCube-v1", max_episode_steps=50)
 class PickCubeEnv(BaseEnv):
+    """
+    **Task Description:**
+    A simple task where the objective is to grasp a red cube and move it to a target goal position.
+
+    **Randomizations:**
+    - the cube's xy position is randomized on top of a table in the region [0.1, 0.1] x [-0.1, -0.1]. It is placed flat on the table
+    - the cube's z-axis rotation is randomized to a random angle
+    - the target goal position (marked by a green sphere) of the cube has its xy position randomized in the region [0.1, 0.1] x [-0.1, -0.1] and z randomized in [0, 0.3]
+
+    **Success Conditions:**
+    - the cube position is within `goal_thresh` (default 0.025m) euclidean distance of the goal position
+    - the robot is static (q velocity < 0.2)
+    """
 
     _sample_video_link = "https://github.com/haosulab/ManiSkill/raw/main/figures/environment_demos/PickCube-v1_rt.mp4"
     SUPPORTED_ROBOTS = [
         "panda",
         "fetch",
         "xarm6_robotiq",
-        "so100",
-        "widowxai",
     ]
-    agent: Union[Panda, Fetch, XArm6Robotiq, SO100, WidowXAI]
+    agent: Union[Panda, Fetch, XArm6Robotiq]
+    cube_half_size = 0.02
     goal_thresh = 0.025
-    cube_spawn_half_size = 0.05
-    cube_spawn_center = (0, 0)
 
     def __init__(self, *args, robot_uids="panda", robot_init_qpos_noise=0.02, **kwargs):
         self.robot_init_qpos_noise = robot_init_qpos_noise
-        if robot_uids in PICK_CUBE_CONFIGS:
-            cfg = PICK_CUBE_CONFIGS[robot_uids]
-        else:
-            cfg = PICK_CUBE_CONFIGS["panda"]
-        self.cube_half_size = cfg["cube_half_size"]
-        self.goal_thresh = cfg["goal_thresh"]
-        self.cube_spawn_half_size = cfg["cube_spawn_half_size"]
-        self.cube_spawn_center = cfg["cube_spawn_center"]
-        self.max_goal_height = cfg["max_goal_height"]
-        self.sensor_cam_eye_pos = cfg["sensor_cam_eye_pos"]
-        self.sensor_cam_target_pos = cfg["sensor_cam_target_pos"]
-        self.human_cam_eye_pos = cfg["human_cam_eye_pos"]
-        self.human_cam_target_pos = cfg["human_cam_target_pos"]
+
+        self._camera_width = None
+        self._camera_height = None
+        if "camera_width" in kwargs:
+            assert "camera_height" in kwargs
+            self._camera_width = kwargs.pop("camera_width")
+            self._camera_height = kwargs.pop("camera_height")
+
         super().__init__(*args, robot_uids=robot_uids, **kwargs)
 
     @property
     def _default_sensor_configs(self):
-        pose = sapien_utils.look_at(
-            eye=self.sensor_cam_eye_pos, target=self.sensor_cam_target_pos
-        )
-        return [CameraConfig("base_camera", pose, 128, 128, np.pi / 2, 0.01, 100)]
+        pose = sapien_utils.look_at(eye=[0.3, 0, 0.6], target=[-0.1, 0, 0.1])
+        if self._camera_width is None:
+            return [CameraConfig("base_camera", pose, 128, 128, np.pi / 2, 0.01, 100)]
+        else:
+            return [CameraConfig("base_camera", pose, self._camera_width, self._camera_height, np.pi / 2, 0.01, 100)]
 
     @property
     def _default_human_render_camera_configs(self):
-        pose = sapien_utils.look_at(
-            eye=self.human_cam_eye_pos, target=self.human_cam_target_pos
-        )
+        pose = sapien_utils.look_at([0.6, 0.7, 0.6], [0.0, 0.0, 0.35])
         return CameraConfig("render_camera", pose, 512, 512, 1, 0.01, 100)
 
     def _load_agent(self, options: dict):
@@ -108,38 +97,27 @@ class PickCubeEnv(BaseEnv):
             b = len(env_idx)
             self.table_scene.initialize(env_idx)
             xyz = torch.zeros((b, 3))
-            xyz[:, :2] = (
-                torch.rand((b, 2)) * self.cube_spawn_half_size * 2
-                - self.cube_spawn_half_size
-            )
-            xyz[:, 0] += self.cube_spawn_center[0]
-            xyz[:, 1] += self.cube_spawn_center[1]
-
+            xyz[:, :2] = torch.rand((b, 2)) * 0.2 - 0.1
             xyz[:, 2] = self.cube_half_size
             qs = randomization.random_quaternions(b, lock_x=True, lock_y=True)
             self.cube.set_pose(Pose.create_from_pq(xyz, qs))
 
             goal_xyz = torch.zeros((b, 3))
-            goal_xyz[:, :2] = (
-                torch.rand((b, 2)) * self.cube_spawn_half_size * 2
-                - self.cube_spawn_half_size
-            )
-            goal_xyz[:, 0] += self.cube_spawn_center[0]
-            goal_xyz[:, 1] += self.cube_spawn_center[1]
-            goal_xyz[:, 2] = torch.rand((b)) * self.max_goal_height + xyz[:, 2]
+            goal_xyz[:, :2] = torch.rand((b, 2)) * 0.2 - 0.1
+            goal_xyz[:, 2] = torch.rand((b)) * 0.3 + xyz[:, 2]
             self.goal_site.set_pose(Pose.create_from_pq(goal_xyz))
 
-    def _get_obs_extra(self, info: dict):
+    def _get_obs_extra(self, info: Dict):
         # in reality some people hack is_grasped into observations by checking if the gripper can close fully or not
         obs = dict(
             is_grasped=info["is_grasped"],
-            tcp_pose=self.agent.tcp_pose.raw_pose,
+            tcp_pose=self.agent.tcp.pose.raw_pose,
             goal_pos=self.goal_site.pose.p,
         )
         if "state" in self.obs_mode:
             obs.update(
                 obj_pose=self.cube.pose.raw_pose,
-                tcp_to_obj_pos=self.cube.pose.p - self.agent.tcp_pose.p,
+                tcp_to_obj_pos=self.cube.pose.p - self.agent.tcp.pose.p,
                 obj_to_goal_pos=self.goal_site.pose.p - self.cube.pose.p,
             )
         return obs
@@ -158,9 +136,9 @@ class PickCubeEnv(BaseEnv):
             "is_grasped": is_grasped,
         }
 
-    def compute_dense_reward(self, obs: Any, action: torch.Tensor, info: dict):
+    def compute_dense_reward(self, obs: Any, action: torch.Tensor, info: Dict):
         tcp_to_obj_dist = torch.linalg.norm(
-            self.cube.pose.p - self.agent.tcp_pose.p, axis=1
+            self.cube.pose.p - self.agent.tcp.pose.p, axis=1
         )
         reaching_reward = 1 - torch.tanh(5 * tcp_to_obj_dist)
         reward = reaching_reward
@@ -174,43 +152,20 @@ class PickCubeEnv(BaseEnv):
         place_reward = 1 - torch.tanh(5 * obj_to_goal_dist)
         reward += place_reward * is_grasped
 
-        qvel = self.agent.robot.get_qvel()
-        if self.robot_uids in ["panda", "widowxai"]:
-            qvel = qvel[..., :-2]
-        elif self.robot_uids == "so100":
-            qvel = qvel[..., :-1]
-        static_reward = 1 - torch.tanh(5 * torch.linalg.norm(qvel, axis=1))
+        qvel_without_gripper = self.agent.robot.get_qvel()
+        if self.robot_uids == "xarm6_robotiq":
+            qvel_without_gripper = qvel_without_gripper[..., :-6]
+        elif self.robot_uids == "panda":
+            qvel_without_gripper = qvel_without_gripper[..., :-2]
+        static_reward = 1 - torch.tanh(
+            5 * torch.linalg.norm(qvel_without_gripper, axis=1)
+        )
         reward += static_reward * info["is_obj_placed"]
 
         reward[info["success"]] = 5
         return reward
 
     def compute_normalized_dense_reward(
-        self, obs: Any, action: torch.Tensor, info: dict
+        self, obs: Any, action: torch.Tensor, info: Dict
     ):
         return self.compute_dense_reward(obs=obs, action=action, info=info) / 5
-
-
-PickCubeEnv.__doc__ = PICK_CUBE_DOC_STRING.format(robot_id="Panda")
-
-
-@register_env("PickCubeSO100-v1", max_episode_steps=50)
-class PickCubeSO100Env(PickCubeEnv):
-    _sample_video_link = "https://github.com/haosulab/ManiSkill/raw/main/figures/environment_demos/PickCubeSO100-v1_rt.mp4"
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, robot_uids="so100", **kwargs)
-
-
-PickCubeSO100Env.__doc__ = PICK_CUBE_DOC_STRING.format(robot_id="SO100")
-
-
-@register_env("PickCubeWidowXAI-v1", max_episode_steps=50)
-class PickCubeWidowXAIEnv(PickCubeEnv):
-    _sample_video_link = "https://github.com/haosulab/ManiSkill/raw/main/figures/environment_demos/PickCubeWidowXAI-v1_rt.mp4"
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, robot_uids="widowxai", **kwargs)
-
-
-PickCubeWidowXAIEnv.__doc__ = PICK_CUBE_DOC_STRING.format(robot_id="WidowXAI")
--- mani_skill/envs/tasks/tabletop/pick_cube_v2.py
+++ mani_skill/envs/tasks/tabletop/pick_cube_v2.py
@@ -1,31 +1,228 @@
 import numpy as np
 
+import torch
+import sapien
+
+import mani_skill.envs.utils.randomization as randomization
 from mani_skill.envs.tasks.tabletop.pick_cube import PickCubeEnv
 from mani_skill.sensors.camera import CameraConfig
 from mani_skill.utils import sapien_utils
 from mani_skill.utils.registration import register_env
+from mani_skill.utils.scene_builder.table import TableSceneBuilder
+from mani_skill.utils.structs.pose import Pose
+from mani_skill.utils.building import actors
 
-DEFAULT_CAMERA_WIDTH = 128
-DEFAULT_CAMERA_HEIGHT = 128
+REALSENSE_DEPTH_FOV_VERTICAL_RAD = 58.0 * np.pi / 180
+REALSENSE_DEPTH_FOV_HORIZONTAL_RAD = 87.0 * np.pi / 180
+
 
 @register_env("PickCube-v2", max_episode_steps=100)
 class PickCubeV2Env(PickCubeEnv):
     """
     **Task Description:**
-    Nearly exacty copy of PickCubeEnv, but with 3 cameras instead of 1.
+    Nearly exacty copy of PickCubeEnv, but with the following change:
+        1. 3 cameras instead of 1
+        2. Cameras have a higher resolution
+        3. Target position is fixed to (0.05, 0.05, 0.25)
+        4. Goal_thresh is the cube half size plus a small margin
+
+    Distractor axes:
+        1. Cube visual. In build_cube(...) add args to RenderMaterial:
+            builder.add_box_visual(
+                half_size=[half_size] * 3,
+                material=sapien.render.RenderMaterial(
+                    base_color=color,
+                ),
+            )
+            (https://sapien.ucsd.edu/docs/latest/apidoc/sapien.core.html#sapien.core.pysapien.RenderMaterial)
+        2.
     """
     def __init__(self, *args, robot_uids="panda", robot_init_qpos_noise=0.02, **kwargs):
-        self._camera_width = kwargs.pop("camera_width", DEFAULT_CAMERA_WIDTH)
-        self._camera_height = kwargs.pop("camera_height", DEFAULT_CAMERA_HEIGHT)
+        assert "camera_width" in kwargs, "camera_width must be provided"
+        assert "camera_height" in kwargs, "camera_height must be provided"
+        # assert "distractor_specification" in kwargs, "distractor_specification must be provided"
+        self._camera_width = kwargs.pop("camera_width")
+        self._camera_height = kwargs.pop("camera_height")
+        # self._distractor_specification = kwargs.pop("distractor_specification")
+
+        self._distractor_spheres = []
+        self._n_distractor_spheres = 0
+        self._distractor_spheres_radius_range = (0.01, 0.025)
+        self._distractor_spheres_color_range = [[0.25, 0.25, 0.25], [1.0, 1.0, 1.0]]
+
+        # Just visualizing the coordinate axes
+        self.coordinate_axes_pts = []
+        self.target_site_xy_projection = None
+
+        # Env configuration
+        self.cube_half_size = 0.02
+        self.goal_thresh = self.cube_half_size + 0.04
+
         super().__init__(*args, robot_uids=robot_uids, robot_init_qpos_noise=robot_init_qpos_noise, **kwargs)
+        # print("Initialized PickCubeV2Env")
+
+    def _initialize_episode(self, env_idx: torch.Tensor, options: dict):
+        # print("Initializing episode, env_idx:", env_idx)
+        with torch.device(self.device):
+            b = len(env_idx)
+            self.table_scene.initialize(env_idx)
+            xyz = torch.zeros((b, 3))
+            xyz[:, :2] = torch.rand((b, 2)) * 0.2 - 0.1
+
+            # TEMP DEBUGGING - Fixed cube start position
+            # xyz[:, 0] = -0.08
+            # xyz[:, 1] = -0.08
+            # END OF: TEMP DEBUGGING
+
+            xyz[:, 2] = self.cube_half_size
+            # print(f"Setting cube position to {xyz * 100} cm")
+            qs = randomization.random_quaternions(b, lock_x=True, lock_y=True)
+            self.cube.set_pose(Pose.create_from_pq(xyz, qs))
+
+            # Fixed target position
+            goal_xyz = torch.zeros((b, 3))
+            goal_xyz[:, 0] = 0.05
+            goal_xyz[:, 1] = 0.05
+            goal_xyz[:, 2] = 0.25
+            self.goal_site.set_pose(Pose.create_from_pq(goal_xyz))
+
+            # Coordinate axes
+            for i in range(3):
+                for j in range(3):
+                    coord_pos = torch.zeros((b, 3))
+                    coord_pos[:, i] = [0.1, 0.05, 0.01][j]
+                    self.coordinate_axes_pts[i*3 + j].set_pose(Pose.create_from_pq(p=coord_pos))
+
+            # Target site xy projection
+            target_site_xy_projection_pos = goal_xyz.clone()
+            target_site_xy_projection_pos[:, 2] = 0.0
+            self.target_site_xy_projection.set_pose(Pose.create_from_pq(p=target_site_xy_projection_pos))
+
+            # New distractor spheres
+            for i in range(self._n_distractor_spheres):
+                sphere_xyz = torch.zeros((b, 3))
+                sphere_xyz[:, :2] = torch.rand((b, 2)) * 0.2 - 0.1
+                sphere_xyz[:, 2] = 0.05
+                self._distractor_spheres[i].set_pose(Pose.create_from_pq(p=sphere_xyz))
+
+
+    def _load_scene(self, options: dict):
+        self.table_scene = TableSceneBuilder(
+            self, robot_init_qpos_noise=self.robot_init_qpos_noise
+        )
+        self.table_scene.build()
+        self.cube = actors.build_cube(
+            self.scene,
+            half_size=self.cube_half_size,
+            color=[1, 0, 0, 1],
+            name="cube",
+            initial_pose=sapien.Pose(p=[0, 0, self.cube_half_size]),
+        )
+        self.goal_site = actors.build_sphere(
+            self.scene,
+            radius=self.goal_thresh,
+            color=[0, 1, 0, 0.75],
+            name="goal_site",
+            body_type="kinematic",
+            add_collision=False,
+            initial_pose=sapien.Pose(),
+        )
+        self._hidden_objects.append(self.goal_site)
+
+        # Add coordinate axis
+        for i in range(3):
+            color = [[1, 0, 0, 1], [0, 1, 0, 1], [0, 0, 1, 1]][i]
+            for j in range(3):
+                self.coordinate_axes_pts.append(actors.build_sphere(
+                    self.scene,
+                    radius=0.0035,
+                    color=color,
+                    name=f"coordinate_axis_point_{i}_{j}",
+                    body_type="kinematic",
+                    add_collision=False,
+                    initial_pose=sapien.Pose(),
+                ))
+        self.target_site_xy_projection = actors.build_sphere(
+            self.scene,
+            radius=0.005,
+            color=[0, 1, 0, 0.75],
+            name="target_site_xy_projection",
+            body_type="kinematic",
+            add_collision=False,
+            initial_pose=sapien.Pose(),
+        )
+
+
+        # New distractor spheres
+        for i in range(self._n_distractor_spheres):
+            radius_i = np.random.uniform(*self._distractor_spheres_radius_range)
+            self._distractor_spheres.append(actors.build_sphere(
+                self.scene,
+                initial_pose=sapien.Pose(p=[0.1, 0.1, radius_i]),
+                name=f"distractor_sphere_{i}",
+                radius=radius_i,
+                color=np.random.uniform(*self._distractor_spheres_color_range).tolist() + [1.0], # alpha=1.0
+            ))
+
+
+    @property
+    def _default_human_render_camera_configs(self):
+        """ Configures the human render camera.
+
+        Shader options:
+            minimal: The fastest shader with minimal GPU memory usage. Note that the background will always be black (normally it is the color of the ambient light)
+            default: A balance between speed and texture availability
+            rt: A shader optimized for photo-realistic rendering via ray-tracing
+            rt-med: Same as rt but runs faster with slightly lower quality
+            rt-fast: Same as rt-med but runs faster with slightly lower quality
+
+            -> https://maniskill.readthedocs.io/en/latest/user_guide/concepts/sensors.html#shaders-and-textures
+        """
+
+        pose = sapien_utils.look_at([0.35, 0.45, 0.4], [0.0, 0.0, 0.15])
+        # pose = sapien_utils.look_at([0.3, 0.4, 0.4], [0.0, 0.0, 0.25])
+        # SHADER = "rt" # doesn't work with parallel rendering
+        SHADER = "default"
+        # SHADER = "minimal" # negligible time difference between 'default' and 'minimal', so 
+        # print(f"Using shader '{SHADER}' for human render camera")
+        return CameraConfig("render_camera", pose=pose, width=1264, height=1264, fov=np.pi / 3, near=0.01, far=100, shader_pack=SHADER)
 
     @property
     def _default_sensor_configs(self):
         pose_center = sapien_utils.look_at(eye=[0.3, 0, 0.6], target=[-0.1, 0, 0.1])
         pose_left = sapien_utils.look_at(eye=[0.0, -0.3, 0.6], target=[-0.1, 0, 0.1])
         pose_right = sapien_utils.look_at(eye=[0.0, 0.3, 0.6], target=[-0.1, 0, 0.1])
+        SHADER = "default"
+        # print(f"Using shader '{SHADER}' for sensor cameras")
         return [
-            CameraConfig("camera_center", pose_center, self._camera_width, self._camera_height, np.pi / 2, 0.01, 100),
-            CameraConfig("camera_left", pose_left, self._camera_width, self._camera_height, np.pi / 2, 0.01, 100),
-            CameraConfig("camera_right", pose_right, self._camera_width, self._camera_height, np.pi / 2, 0.01, 100),
+            CameraConfig(
+                uid="camera_center",
+                pose=pose_center,
+                width=self._camera_width,
+                height=self._camera_height,
+                fov=np.pi / 2,
+                near=0.01,
+                far=100,
+                shader_pack=SHADER,
+            ),
+            CameraConfig(
+                uid="camera_left",
+                pose=pose_left,
+                width=self._camera_width,
+                height=self._camera_height,
+                fov=np.pi / 2,
+                near=0.01,
+                far=100,
+                shader_pack=SHADER,
+            ),
+            CameraConfig(
+                uid="camera_right",
+                pose=pose_right,
+                width=self._camera_width,
+                height=self._camera_height,
+                fov=np.pi / 2,
+                near=0.01,
+                far=100,
+                shader_pack=SHADER,
+            ),
         ]
