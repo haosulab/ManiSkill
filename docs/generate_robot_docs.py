4db11e999c3356eb48b8c70b6d1846a0d1edbac0
Stone Tao <stonezt2019@gmail.com>
2025-04-04 16:10:43 -0700

    new image locations

--- docs/generate_robot_docs.py
+++ docs/generate_robot_docs.py
@@ -2,6 +2,7 @@
 Code to automatically generate robot documentation from the robot classes exported in the mani_skill.agents.robots module.
 """
 
+from importlib.metadata import requires
 from typing import List
 
 import numpy as np
@@ -17,7 +18,7 @@ import mani_skill.agents.robots as robots
 from mani_skill.agents.base_agent import BaseAgent
 from mani_skill.envs.tasks.empty_env import EmptyEnv
 from mani_skill.utils import sapien_utils
-from mani_skill.utils.download_demo import DATASET_SOURCES
+from mani_skill.utils.download_asset import DATA_GROUPS
 import sapien.utils.viewer.entity_window
 
 def capture_images(env: EmptyEnv):
@@ -43,6 +44,8 @@ def capture_images(env: EmptyEnv):
 
 def main():
     print("Classes exported in mani_skill.agents.robots:")
+    base_dir = Path(__file__).parent / "source/robots"
+
 
     agent_classes: List[BaseAgent] = []
     # Get all attributes in the robots module
@@ -53,94 +56,151 @@ def main():
             if obj.__module__.startswith('mani_skill.agents.robots'):
                 print(f"- {name}")
                 agent_classes.append(obj)
+    robots_table_markdown_str = """
+# Robots
+```{figure} images/robot-grid.png
+```
 
-    for agent in agent_classes:
-        if agent.uid != "xarm6_robotiq":
-            continue
-        try:
-            env = EmptyEnv(robot_uids=agent.uid, render_mode="rgb_array", human_render_camera_configs=dict(shader_pack="rt", width=512, height=512))
-            env.reset()
-            kf = env.agent.keyframes
-            # Get the first keyframe if available
-            if kf and len(kf) > 0:
-                first_keyframe_name = next(iter(kf))
-                first_keyframe = kf[first_keyframe_name]
-                env.agent.robot.set_qpos(first_keyframe.qpos)
-                env.agent.robot.set_pose(first_keyframe.pose)
+This sections here show the already built/modelled robots ready for simulation across a number of different categories. Some of them are displayed above in an empty environment using a predefined keyframe. Note that not all of these robots are used in tasks in ManiSkill, and some are not tuned for maximum efficiency yet or for sim2real transfer. You can generally assume robots that are used in existing tasks in ManiSkill are of the highest quality and already tuned.
+
+To learn about how to load your own custom robots see [the tutorial](../user_guide/tutorials/custom_robots.md).
+"""
+    robots_table_markdown_str += "\n## Robots Table\n"
+    robots_table_markdown_str += "Table of all robots modelled in ManiSkill"
+    robots_table_markdown_str += '\n<table class="table">'
+    robots_table_markdown_str += "\n<thead>"
+    robots_table_markdown_str += '\n<tr class="row-odd">'
+    robots_table_markdown_str += '\n<th class="head"><p>Robot UID</p></th>'
+    robots_table_markdown_str += '\n<th class="head"><p>Preview</p></th>'
+    robots_table_markdown_str += '\n<th class="head"><p>Requires Download</p></th>'
+    robots_table_markdown_str += '\n<th class="head"><p>Quality</p></th>'
+    robots_table_markdown_str += "\n</tr>"
+    robots_table_markdown_str += "\n</thead>"
+    robots_table_markdown_str += "\n<tbody>"
 
 
-            imgs = capture_images(env)
-            cv2.imwrite(f"source/robots/images/{agent.uid}_front.png", cv2.cvtColor(imgs["front"], cv2.COLOR_BGR2RGB))
-            cv2.imwrite(f"source/robots/images/{agent.uid}_side.png", cv2.cvtColor(imgs["side"], cv2.COLOR_BGR2RGB))
+    for row_idx, agent in enumerate(agent_classes):
+        ### generate images of the robot ###
+        # try:
+        #     env = EmptyEnv(robot_uids=agent.uid, render_mode="rgb_array", human_render_camera_configs=dict(shader_pack="rt", width=1024, height=1024))
+        #     env.reset()
+        #     kf = env.agent.keyframes
+        #     # Get the first keyframe if available
+        #     if kf and len(kf) > 0:
+        #         first_keyframe_name = next(iter(kf))
+        #         first_keyframe = kf[first_keyframe_name]
+        #         env.agent.robot.set_qpos(first_keyframe.qpos)
+        #         env.agent.robot.set_pose(first_keyframe.pose)
 
-            red_mat = sapien.render.RenderMaterial(base_color=[1, 0, 0, 1])
-            green_mat = sapien.render.RenderMaterial(base_color=[0, 1, 0, 1])
-            blue_mat = sapien.render.RenderMaterial(base_color=[0, 0, 1, 1])
-            def add_collision_visual(entity: sapien.Entity):
-                new_visual = sapien.render.RenderBodyComponent()
-                new_visual.disable_render_id()  # avoid it interfere with visual id counting
-                for c in entity.components:
-                    if isinstance(c, sapien.physx.PhysxRigidBaseComponent):
-                        for s in c.collision_shapes:
-                            if isinstance(s, sapien.physx.PhysxCollisionShapeSphere):
-                                vs = sapien.render.RenderShapeSphere(s.radius, blue_mat)
 
-                            elif isinstance(s, sapien.physx.PhysxCollisionShapeBox):
-                                vs = sapien.render.RenderShapeBox(s.half_size, blue_mat)
+        #     imgs = capture_images(env)
+        #     Path(f"source/_static/robot_images/{agent.uid}").mkdir(parents=True, exist_ok=True)
+        #     cv2.imwrite(f"source/_static/robot_images/{agent.uid}/front_visual.png", cv2.cvtColor(imgs["front"], cv2.COLOR_BGR2RGB))
+        #     cv2.imwrite(f"source/_static/robot_images/{agent.uid}/side_visual.png", cv2.cvtColor(imgs["side"], cv2.COLOR_BGR2RGB))
 
-                            elif isinstance(s, sapien.physx.PhysxCollisionShapeCapsule):
-                                vs = sapien.render.RenderShapeCapsule(
-                                    s.radius, s.half_length, blue_mat
-                                )
+        #     cv2.imwrite(f"source/_static/robot_images/{agent.uid}/thumbnail.png", cv2.cvtColor(cv2.resize(imgs["side"], (256, 256)), cv2.COLOR_BGR2RGB))
 
-                            elif isinstance(s, sapien.physx.PhysxCollisionShapeConvexMesh):
-                                vs = sapien.render.RenderShapeTriangleMesh(
-                                    s.vertices,
-                                    s.triangles,
-                                    np.zeros((0, 3)),
-                                    np.zeros((0, 2)),
-                                    green_mat,
-                                )
-                                vs.scale = s.scale
+        #     red_mat = sapien.render.RenderMaterial(base_color=[1, 0, 0, 1])
+        #     green_mat = sapien.render.RenderMaterial(base_color=[0, 1, 0, 1])
+        #     blue_mat = sapien.render.RenderMaterial(base_color=[0, 0, 1, 1])
+        #     def add_collision_visual(entity: sapien.Entity):
+        #         new_visual = sapien.render.RenderBodyComponent()
+        #         new_visual.disable_render_id()  # avoid it interfere with visual id counting
+        #         for c in entity.components:
+        #             if isinstance(c, sapien.physx.PhysxRigidBaseComponent):
+        #                 for s in c.collision_shapes:
+        #                     if isinstance(s, sapien.physx.PhysxCollisionShapeSphere):
+        #                         vs = sapien.render.RenderShapeSphere(s.radius, blue_mat)
 
-                            elif isinstance(s, sapien.physx.PhysxCollisionShapeTriangleMesh):
-                                vs = sapien.render.RenderShapeTriangleMesh(
-                                    s.vertices,
-                                    s.triangles,
-                                    np.zeros((0, 3)),
-                                    np.zeros((0, 2)),
-                                    red_mat,
-                                )
-                                vs.scale = s.scale
+        #                     elif isinstance(s, sapien.physx.PhysxCollisionShapeBox):
+        #                         vs = sapien.render.RenderShapeBox(s.half_size, blue_mat)
 
-                            elif isinstance(s, sapien.physx.PhysxCollisionShapePlane):
-                                vs = sapien.render.RenderShapePlane([1, 1e4, 1e4], blue_mat)
+        #                     elif isinstance(s, sapien.physx.PhysxCollisionShapeCapsule):
+        #                         vs = sapien.render.RenderShapeCapsule(
+        #                             s.radius, s.half_length, blue_mat
+        #                         )
 
-                            elif isinstance(s, sapien.physx.PhysxCollisionShapeCylinder):
-                                vs = sapien.render.RenderShapeCylinder(
-                                    s.radius, s.half_length, green_mat
-                                )
+        #                     elif isinstance(s, sapien.physx.PhysxCollisionShapeConvexMesh):
+        #                         vs = sapien.render.RenderShapeTriangleMesh(
+        #                             s.vertices,
+        #                             s.triangles,
+        #                             np.zeros((0, 3)),
+        #                             np.zeros((0, 2)),
+        #                             green_mat,
+        #                         )
+        #                         vs.scale = s.scale
 
-                            else:
-                                raise Exception(
-                                    "invalid collision shape, this code should be unreachable."
-                                )
+        #                     elif isinstance(s, sapien.physx.PhysxCollisionShapeTriangleMesh):
+        #                         vs = sapien.render.RenderShapeTriangleMesh(
+        #                             s.vertices,
+        #                             s.triangles,
+        #                             np.zeros((0, 3)),
+        #                             np.zeros((0, 2)),
+        #                             red_mat,
+        #                         )
+        #                         vs.scale = s.scale
 
-                            vs.local_pose = s.local_pose
+        #                     elif isinstance(s, sapien.physx.PhysxCollisionShapePlane):
+        #                         vs = sapien.render.RenderShapePlane([1, 1e4, 1e4], blue_mat)
 
-                            new_visual.attach(vs)
+        #                     elif isinstance(s, sapien.physx.PhysxCollisionShapeCylinder):
+        #                         vs = sapien.render.RenderShapeCylinder(
+        #                             s.radius, s.half_length, green_mat
+        #                         )
 
-                entity.add_component(new_visual)
-                new_visual.set_property("shadeFlat", 1)
-            for link in env.agent.robot.links:
-                for c in link._objs[0].entity.components:
-                    if isinstance(c, sapien.render.RenderBodyComponent):
-                        c.disable()
-                add_collision_visual(link._objs[0].entity)
-            imgs = capture_images(env)
-            cv2.imwrite(f"source/robots/images/{agent.uid}_front_collision.png", cv2.cvtColor(imgs["front"], cv2.COLOR_BGR2RGB))
-            cv2.imwrite(f"source/robots/images/{agent.uid}_side_collision.png", cv2.cvtColor(imgs["side"], cv2.COLOR_BGR2RGB))
-        except Exception as e:
-            print(f"Error processing {agent.uid}, skipping: {e}")
+        #                     else:
+        #                         raise Exception(
+        #                             "invalid collision shape, this code should be unreachable."
+        #                         )
+
+        #                     vs.local_pose = s.local_pose
+
+        #                     new_visual.attach(vs)
+
+        #         entity.add_component(new_visual)
+        #         new_visual.set_property("shadeFlat", 1)
+        #     for link in env.agent.robot.links:
+        #         for c in link._objs[0].entity.components:
+        #             if isinstance(c, sapien.render.RenderBodyComponent):
+        #                 c.disable()
+        #         add_collision_visual(link._objs[0].entity)
+        #     imgs = capture_images(env)
+        #     cv2.imwrite(f"source/_static/robot_images/{agent.uid}/front_collision.png", cv2.cvtColor(imgs["front"], cv2.COLOR_BGR2RGB))
+        #     cv2.imwrite(f"source/_static/robot_images/{agent.uid}/side_collision.png", cv2.cvtColor(imgs["side"], cv2.COLOR_BGR2RGB))
+        # except Exception as e:
+        #     print(f"Error processing {agent.uid}, skipping: {e}")
+
+        ### generate robot docs ###
+        requires_downloading_assets = False
+        if agent.uid in DATA_GROUPS:
+            requires_downloading_assets = True
+
+        urdf_path = agent.urdf_path
+        agent_class_code_link = f"https://github.com/haosulab/ManiSkill/blob/main/{agent.__module__.replace('.', '/') + '.py'}"
+        if requires_downloading_assets:
+            # note (stao): asset download location might move away from github to another place in the future
+            pass
+
+
+        robots_table_markdown_str += (
+            f"\n<tr class=\"row-{'even' if row_idx % 2 == 1 else 'odd'}\">"
+        )
+        robots_table_markdown_str += (
+            f'\n<td><p><a href="#{agent.uid.lower()}">{agent.uid}</a></p></td>'
+        )
+        thumbnail_path = f"/_static/robot_images/{agent.uid}/thumbnail.png"
+        thumbnail = f"<img style='min-width:min(50%, 100px);max-width:100px;height:auto' src='{thumbnail_path}' alt='{agent.uid}'>"
+        robots_table_markdown_str += (
+            f"\n<td><div style='display:flex;gap:4px;align-items:center'>{thumbnail}</div></td>"
+        )
+        demos = "✅" if requires_downloading_assets else "❌"
+        robots_table_markdown_str += (f"\n<td><p>{demos}</p></td>")
+        robots_table_markdown_str += (f"\n<td><p>C</p></td>")
+        robots_table_markdown_str += (f"\n</tr>")
+    robots_table_markdown_str += "\n</tbody>\n</table>"
+    with open(
+        f"{base_dir}/index.md", "w"
+    ) as f:
+        f.write(robots_table_markdown_str)
 if __name__ == "__main__":
     main()
--- docs/source/robots/index.md
+++ docs/source/robots/index.md
@@ -1,22 +1,191 @@
-# Robots
 
+# Robots
 ```{figure} images/robot-grid.png
 ```
-
-
 
 This sections here show the already built/modelled robots ready for simulation across a number of different categories. Some of them are displayed above in an empty environment using a predefined keyframe. Note that not all of these robots are used in tasks in ManiSkill, and some are not tuned for maximum efficiency yet or for sim2real transfer. You can generally assume robots that are used in existing tasks in ManiSkill are of the highest quality and already tuned.
 
 To learn about how to load your own custom robots see [the tutorial](../user_guide/tutorials/custom_robots.md).
 
-
-```{toctree}
-:caption: Directory
-
-mobile_manipulators/index
-quadrupeds/index
-arms/index
-humanoids/index
-dextrous_hands/index
-other/index
-```
+## Robots Table
+Table of all robots modelled in ManiSkill
+<table class="table">
+<thead>
+<tr class="row-odd">
+<th class="head"><p>Robot UID</p></th>
+<th class="head"><p>Preview</p></th>
+<th class="head"><p>Requires Download</p></th>
+<th class="head"><p>Quality</p></th>
+</tr>
+</thead>
+<tbody>
+<tr class="row-odd">
+<td><p><a href="#anymal_c">anymal_c</a></p></td>
+<td><div style='display:flex;gap:4px;align-items:center'><img style='min-width:min(50%, 100px);max-width:100px;height:auto' src='/_static/robot_images/anymal_c/thumbnail.png' alt='anymal_c'></div></td>
+<td><p>✅</p></td>
+<td><p>C</p></td>
+</tr>
+<tr class="row-even">
+<td><p><a href="#allegro_hand_left">allegro_hand_left</a></p></td>
+<td><div style='display:flex;gap:4px;align-items:center'><img style='min-width:min(50%, 100px);max-width:100px;height:auto' src='/_static/robot_images/allegro_hand_left/thumbnail.png' alt='allegro_hand_left'></div></td>
+<td><p>✅</p></td>
+<td><p>C</p></td>
+</tr>
+<tr class="row-odd">
+<td><p><a href="#allegro_hand_right">allegro_hand_right</a></p></td>
+<td><div style='display:flex;gap:4px;align-items:center'><img style='min-width:min(50%, 100px);max-width:100px;height:auto' src='/_static/robot_images/allegro_hand_right/thumbnail.png' alt='allegro_hand_right'></div></td>
+<td><p>✅</p></td>
+<td><p>C</p></td>
+</tr>
+<tr class="row-even">
+<td><p><a href="#allegro_hand_right_touch">allegro_hand_right_touch</a></p></td>
+<td><div style='display:flex;gap:4px;align-items:center'><img style='min-width:min(50%, 100px);max-width:100px;height:auto' src='/_static/robot_images/allegro_hand_right_touch/thumbnail.png' alt='allegro_hand_right_touch'></div></td>
+<td><p>✅</p></td>
+<td><p>C</p></td>
+</tr>
+<tr class="row-odd">
+<td><p><a href="#dclaw">dclaw</a></p></td>
+<td><div style='display:flex;gap:4px;align-items:center'><img style='min-width:min(50%, 100px);max-width:100px;height:auto' src='/_static/robot_images/dclaw/thumbnail.png' alt='dclaw'></div></td>
+<td><p>✅</p></td>
+<td><p>C</p></td>
+</tr>
+<tr class="row-even">
+<td><p><a href="#fetch">fetch</a></p></td>
+<td><div style='display:flex;gap:4px;align-items:center'><img style='min-width:min(50%, 100px);max-width:100px;height:auto' src='/_static/robot_images/fetch/thumbnail.png' alt='fetch'></div></td>
+<td><p>✅</p></td>
+<td><p>C</p></td>
+</tr>
+<tr class="row-odd">
+<td><p><a href="#floating_inspire_hand_right">floating_inspire_hand_right</a></p></td>
+<td><div style='display:flex;gap:4px;align-items:center'><img style='min-width:min(50%, 100px);max-width:100px;height:auto' src='/_static/robot_images/floating_inspire_hand_right/thumbnail.png' alt='floating_inspire_hand_right'></div></td>
+<td><p>✅</p></td>
+<td><p>C</p></td>
+</tr>
+<tr class="row-even">
+<td><p><a href="#floating_panda_gripper">floating_panda_gripper</a></p></td>
+<td><div style='display:flex;gap:4px;align-items:center'><img style='min-width:min(50%, 100px);max-width:100px;height:auto' src='/_static/robot_images/floating_panda_gripper/thumbnail.png' alt='floating_panda_gripper'></div></td>
+<td><p>✅</p></td>
+<td><p>C</p></td>
+</tr>
+<tr class="row-odd">
+<td><p><a href="#floating_robotiq_2f_85_gripper">floating_robotiq_2f_85_gripper</a></p></td>
+<td><div style='display:flex;gap:4px;align-items:center'><img style='min-width:min(50%, 100px);max-width:100px;height:auto' src='/_static/robot_images/floating_robotiq_2f_85_gripper/thumbnail.png' alt='floating_robotiq_2f_85_gripper'></div></td>
+<td><p>✅</p></td>
+<td><p>C</p></td>
+</tr>
+<tr class="row-even">
+<td><p><a href="#googlerobot">googlerobot</a></p></td>
+<td><div style='display:flex;gap:4px;align-items:center'><img style='min-width:min(50%, 100px);max-width:100px;height:auto' src='/_static/robot_images/googlerobot/thumbnail.png' alt='googlerobot'></div></td>
+<td><p>✅</p></td>
+<td><p>C</p></td>
+</tr>
+<tr class="row-odd">
+<td><p><a href="#humanoid">humanoid</a></p></td>
+<td><div style='display:flex;gap:4px;align-items:center'><img style='min-width:min(50%, 100px);max-width:100px;height:auto' src='/_static/robot_images/humanoid/thumbnail.png' alt='humanoid'></div></td>
+<td><p>✅</p></td>
+<td><p>C</p></td>
+</tr>
+<tr class="row-even">
+<td><p><a href="#panda">panda</a></p></td>
+<td><div style='display:flex;gap:4px;align-items:center'><img style='min-width:min(50%, 100px);max-width:100px;height:auto' src='/_static/robot_images/panda/thumbnail.png' alt='panda'></div></td>
+<td><p>✅</p></td>
+<td><p>C</p></td>
+</tr>
+<tr class="row-odd">
+<td><p><a href="#panda_stick">panda_stick</a></p></td>
+<td><div style='display:flex;gap:4px;align-items:center'><img style='min-width:min(50%, 100px);max-width:100px;height:auto' src='/_static/robot_images/panda_stick/thumbnail.png' alt='panda_stick'></div></td>
+<td><p>✅</p></td>
+<td><p>C</p></td>
+</tr>
+<tr class="row-even">
+<td><p><a href="#panda_wristcam">panda_wristcam</a></p></td>
+<td><div style='display:flex;gap:4px;align-items:center'><img style='min-width:min(50%, 100px);max-width:100px;height:auto' src='/_static/robot_images/panda_wristcam/thumbnail.png' alt='panda_wristcam'></div></td>
+<td><p>✅</p></td>
+<td><p>C</p></td>
+</tr>
+<tr class="row-odd">
+<td><p><a href="#stompy">stompy</a></p></td>
+<td><div style='display:flex;gap:4px;align-items:center'><img style='min-width:min(50%, 100px);max-width:100px;height:auto' src='/_static/robot_images/stompy/thumbnail.png' alt='stompy'></div></td>
+<td><p>✅</p></td>
+<td><p>C</p></td>
+</tr>
+<tr class="row-even">
+<td><p><a href="#trifingerpro">trifingerpro</a></p></td>
+<td><div style='display:flex;gap:4px;align-items:center'><img style='min-width:min(50%, 100px);max-width:100px;height:auto' src='/_static/robot_images/trifingerpro/thumbnail.png' alt='trifingerpro'></div></td>
+<td><p>✅</p></td>
+<td><p>C</p></td>
+</tr>
+<tr class="row-odd">
+<td><p><a href="#ur_10e">ur_10e</a></p></td>
+<td><div style='display:flex;gap:4px;align-items:center'><img style='min-width:min(50%, 100px);max-width:100px;height:auto' src='/_static/robot_images/ur_10e/thumbnail.png' alt='ur_10e'></div></td>
+<td><p>✅</p></td>
+<td><p>C</p></td>
+</tr>
+<tr class="row-even">
+<td><p><a href="#unitree_g1">unitree_g1</a></p></td>
+<td><div style='display:flex;gap:4px;align-items:center'><img style='min-width:min(50%, 100px);max-width:100px;height:auto' src='/_static/robot_images/unitree_g1/thumbnail.png' alt='unitree_g1'></div></td>
+<td><p>✅</p></td>
+<td><p>C</p></td>
+</tr>
+<tr class="row-odd">
+<td><p><a href="#unitree_g1_simplified_legs">unitree_g1_simplified_legs</a></p></td>
+<td><div style='display:flex;gap:4px;align-items:center'><img style='min-width:min(50%, 100px);max-width:100px;height:auto' src='/_static/robot_images/unitree_g1_simplified_legs/thumbnail.png' alt='unitree_g1_simplified_legs'></div></td>
+<td><p>✅</p></td>
+<td><p>C</p></td>
+</tr>
+<tr class="row-even">
+<td><p><a href="#unitree_g1_simplified_upper_body">unitree_g1_simplified_upper_body</a></p></td>
+<td><div style='display:flex;gap:4px;align-items:center'><img style='min-width:min(50%, 100px);max-width:100px;height:auto' src='/_static/robot_images/unitree_g1_simplified_upper_body/thumbnail.png' alt='unitree_g1_simplified_upper_body'></div></td>
+<td><p>✅</p></td>
+<td><p>C</p></td>
+</tr>
+<tr class="row-odd">
+<td><p><a href="#unitree_go2">unitree_go2</a></p></td>
+<td><div style='display:flex;gap:4px;align-items:center'><img style='min-width:min(50%, 100px);max-width:100px;height:auto' src='/_static/robot_images/unitree_go2/thumbnail.png' alt='unitree_go2'></div></td>
+<td><p>✅</p></td>
+<td><p>C</p></td>
+</tr>
+<tr class="row-even">
+<td><p><a href="#unitree_h1">unitree_h1</a></p></td>
+<td><div style='display:flex;gap:4px;align-items:center'><img style='min-width:min(50%, 100px);max-width:100px;height:auto' src='/_static/robot_images/unitree_h1/thumbnail.png' alt='unitree_h1'></div></td>
+<td><p>✅</p></td>
+<td><p>C</p></td>
+</tr>
+<tr class="row-odd">
+<td><p><a href="#unitree_h1_simplified">unitree_h1_simplified</a></p></td>
+<td><div style='display:flex;gap:4px;align-items:center'><img style='min-width:min(50%, 100px);max-width:100px;height:auto' src='/_static/robot_images/unitree_h1_simplified/thumbnail.png' alt='unitree_h1_simplified'></div></td>
+<td><p>✅</p></td>
+<td><p>C</p></td>
+</tr>
+<tr class="row-even">
+<td><p><a href="#widowx250s">widowx250s</a></p></td>
+<td><div style='display:flex;gap:4px;align-items:center'><img style='min-width:min(50%, 100px);max-width:100px;height:auto' src='/_static/robot_images/widowx250s/thumbnail.png' alt='widowx250s'></div></td>
+<td><p>✅</p></td>
+<td><p>C</p></td>
+</tr>
+<tr class="row-odd">
+<td><p><a href="#xarm6_nogripper">xarm6_nogripper</a></p></td>
+<td><div style='display:flex;gap:4px;align-items:center'><img style='min-width:min(50%, 100px);max-width:100px;height:auto' src='/_static/robot_images/xarm6_nogripper/thumbnail.png' alt='xarm6_nogripper'></div></td>
+<td><p>✅</p></td>
+<td><p>C</p></td>
+</tr>
+<tr class="row-even">
+<td><p><a href="#xarm6_robotiq">xarm6_robotiq</a></p></td>
+<td><div style='display:flex;gap:4px;align-items:center'><img style='min-width:min(50%, 100px);max-width:100px;height:auto' src='/_static/robot_images/xarm6_robotiq/thumbnail.png' alt='xarm6_robotiq'></div></td>
+<td><p>✅</p></td>
+<td><p>C</p></td>
+</tr>
+<tr class="row-odd">
+<td><p><a href="#xarm6_robotiq_wristcam">xarm6_robotiq_wristcam</a></p></td>
+<td><div style='display:flex;gap:4px;align-items:center'><img style='min-width:min(50%, 100px);max-width:100px;height:auto' src='/_static/robot_images/xarm6_robotiq_wristcam/thumbnail.png' alt='xarm6_robotiq_wristcam'></div></td>
+<td><p>✅</p></td>
+<td><p>C</p></td>
+</tr>
+<tr class="row-even">
+<td><p><a href="#xarm7_ability">xarm7_ability</a></p></td>
+<td><div style='display:flex;gap:4px;align-items:center'><img style='min-width:min(50%, 100px);max-width:100px;height:auto' src='/_static/robot_images/xarm7_ability/thumbnail.png' alt='xarm7_ability'></div></td>
+<td><p>✅</p></td>
+<td><p>C</p></td>
+</tr>
+</tbody>
+</table>
