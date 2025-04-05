c5e0f5cc575ac5906401dcbf161c6a12af6f8198
StoneT2000 <stonezt2019@gmail.com>
2025-04-05 13:11:32 -0700

    work

--- mani_skill/agents/robots/so100/so_100.py
+++ mani_skill/agents/robots/so100/so_100.py
@@ -17,18 +17,15 @@ from mani_skill.utils.structs.actor import Actor
 class SO100(BaseAgent):
     uid = "so100"
     urdf_path = f"{PACKAGE_ASSET_DIR}/robots/so100/SO_5DOF_ARM100_8j/original.urdf"
-    # urdf_config = dict(
-    #     _materials=dict(
-    #         gripper=dict(static_friction=0.3, dynamic_friction=0.3, restitution=0.0)
-    #     ),
-    #     link=dict(
-    #         link_6=dict(material="gripper", patch_radius=0.1, min_patch_radius=0.1),
-    #         gripper=dict(material="gripper", patch_radius=0.1, min_patch_radius=0.1),
-    #     ),
-    # )
-    # load_multiple_collisions = True
-    # self = True
-    # disable_self_collision = True
+    urdf_config = dict(
+        _materials=dict(
+            gripper=dict(static_friction=2, dynamic_friction=2, restitution=0.0)
+        ),
+        link=dict(
+            Fixed_Jaw=dict(material="gripper", patch_radius=0.1, min_patch_radius=0.1),
+            Moving_Jaw=dict(material="gripper", patch_radius=0.1, min_patch_radius=0.1),
+        ),
+    )
 
     keyframes = dict(
         rest=Keyframe(
@@ -73,7 +70,7 @@ class SO100(BaseAgent):
             damping=[1e2] * 6,
             force_limit=100,
             use_delta=True,
-            use_target=True,
+            use_target=False,
         )
         pd_joint_target_delta_pos = copy.deepcopy(pd_joint_delta_pos)
         pd_joint_target_delta_pos.use_target = True
@@ -85,48 +82,44 @@ class SO100(BaseAgent):
         )
         return deepcopy_dict(controller_configs)
 
-    def set_colors(self, base_color=None, motor_color=None):
-        """
-        basecolor:  RGBA length 4
-        motorcolor: RBGA length 4
-        """
-        if base_color is not None:
-            self.robot_chassis_colors = base_color
-        if motor_color is not None:
-            self.robot_motor_colors = motor_color
-        for link in self.robot.links:
-            for i, obj in enumerate(link._objs):
-                rb_comp = obj.entity.find_component_by_type(
-                    sapien.render.RenderBodyComponent
-                )
-                if rb_comp is not None:
-                    rb_comp: sapien.render.RenderBodyComponent
-                    meshes_to_modify = [
-                        x for x in rb_comp.render_shapes if "chassis" in x.name
-                    ]
-                    for mesh in meshes_to_modify:
-                        if isinstance(self.robot_chassis_colors[0], list):
-                            color = self.robot_chassis_colors[i]
-                        else:
-                            color = self.robot_chassis_colors
-                        mesh.material.base_color = color
+    def _after_loading_articulation(self):
+        super()._after_loading_articulation()
+        self.finger1_link = self.robot.links_map["Fixed_Jaw"]
+        self.finger2_link = self.robot.links_map["Moving_Jaw"]
+        self.finger1_tip = self.robot.links_map["Fixed_Jaw_tip"]
+        self.finger2_tip = self.robot.links_map["Moving_Jaw_tip"]
 
-                    other_meshes_to_modify = [
-                        x for x in rb_comp.render_shapes if "motor" in x.name
-                    ]
-                    for mesh in other_meshes_to_modify:
-                        if isinstance(self.robot_motor_colors[0], list):
-                            color = self.robot_motor_colors[i]
-                        else:
-                            color = self.robot_motor_colors
-                        for part in mesh.parts:
-                            part.material.base_color = color
+    @property
+    def tcp_pos(self):
+        # computes the tool center point as the mid point between the the fixed and moving jaw's tips
+        return (self.finger1_tip.pose.p + self.finger2_tip.pose.p) / 2
 
-    # def _after_loading_articulation(self):
-    #     super()._after_loading_articulation()
-    #     self.set_colors()
-    #     self.finger1_link = self.robot.links_map["gripper"]
-    #     self.finger2_link = self.robot.links_map["link_6"]
-    #     self.tcp = self.robot.links_map["gripper_tcp"]
-    #     self.tcp2 = self.robot.links_map["gripper_tcp2"]
-    #     self.back_tcp = self.robot.links_map["back_tcp"]
+    def is_grasping(self, object: Actor, min_force=0.5, max_angle=110):
+        """Check if the robot is grasping an object
+
+        Args:
+            object (Actor): The object to check if the robot is grasping
+            min_force (float, optional): Minimum force before the robot is considered to be grasping the object in Newtons. Defaults to 0.5.
+            max_angle (int, optional): Maximum angle of contact to consider grasping. Defaults to 85.
+        """
+        l_contact_forces = self.scene.get_pairwise_contact_forces(
+            self.finger1_link, object
+        )
+        r_contact_forces = self.scene.get_pairwise_contact_forces(
+            self.finger2_link, object
+        )
+        lforce = torch.linalg.norm(l_contact_forces, axis=1)
+        rforce = torch.linalg.norm(r_contact_forces, axis=1)
+
+        # direction to open the gripper
+        ldirection = self.finger1_link.pose.to_transformation_matrix()[..., :3, 1]
+        rdirection = -self.finger2_link.pose.to_transformation_matrix()[..., :3, 1]
+        langle = common.compute_angle_between(ldirection, l_contact_forces)
+        rangle = common.compute_angle_between(rdirection, r_contact_forces)
+        lflag = torch.logical_and(
+            lforce >= min_force, torch.rad2deg(langle) <= max_angle
+        )
+        rflag = torch.logical_and(
+            rforce >= min_force, torch.rad2deg(rangle) <= max_angle
+        )
+        return torch.logical_and(lflag, rflag)
