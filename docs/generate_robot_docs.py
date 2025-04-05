3e3a9053ebfaa82570340704eb11f2503e2ecd9f
StoneT2000 <stonezt2019@gmail.com>
2025-04-04 21:44:13 -0700

    [Docs] fix broken image link again

--- docs/generate_robot_docs.py
+++ docs/generate_robot_docs.py
@@ -68,7 +68,7 @@ def main():
                 agent_classes.append(obj)
     robot_index_markdown_str = GLOBAL_ROBOT_DOCS_HEADER + """
 # Robots
-<img src="/_static/robot_images/robot-grid.png" alt="Robot Grid" style="width: 100%; height: auto;">
+<img src="../_static/robot_images/robot-grid.png" alt="Robot Grid" style="width: 100%; height: auto;">
 
 
 This sections here show the already built/modelled robots ready for simulation across a number of different categories. Some of them are displayed above in an empty environment using a predefined keyframe. Note that not all of these robots are used in tasks in ManiSkill, and some are not tuned for maximum efficiency yet or for sim2real transfer. You can generally assume robots that are used in existing tasks in ManiSkill are of the highest quality and already tuned.
--- docs/source/robots/index.md
+++ docs/source/robots/index.md
@@ -1,7 +1,7 @@
 <!-- THIS IS ALL GENERATED DOCUMENTATION via generate_robot_docs.py. DO NOT MODIFY THIS FILE DIRECTLY. -->
 
 # Robots
-<img src="/_static/robot_images/robot-grid.png" alt="Robot Grid" style="width: 100%; height: auto;">
+<img src="../_static/robot_images/robot-grid.png" alt="Robot Grid" style="width: 100%; height: auto;">
 
 
 This sections here show the already built/modelled robots ready for simulation across a number of different categories. Some of them are displayed above in an empty environment using a predefined keyframe. Note that not all of these robots are used in tasks in ManiSkill, and some are not tuned for maximum efficiency yet or for sim2real transfer. You can generally assume robots that are used in existing tasks in ManiSkill are of the highest quality and already tuned.
