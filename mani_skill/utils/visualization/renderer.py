9a2481a8653bb90f5630e7a8db2bf7a36502dc4b
arth-shukla <44527268+arth-shukla@users.noreply.github.com>
2025-04-18 13:30:05 -0700

    demo_manual_control_continuous play script

--- mani_skill/utils/visualization/renderer.py
+++ mani_skill/utils/visualization/renderer.py
@@ -15,22 +15,32 @@ class ImageRenderer:
         self._image = None
         self.last_event = None
         self.wait_for_button_press = wait_for_button_press
+        self.pressed_keys = set()
 
-    def event_handler(self, event):
+    def key_press_handler(self, event):
         self.last_event = event
+        self.pressed_keys.add(event.key)
         if event.key in ["q", "escape"]:
             sys.exit(0)
+
+    def key_release_handler(self, event):
+        if event.key in self.pressed_keys:
+            self.pressed_keys.remove(event.key)
 
     def __call__(self, buffer):
         if not self._image:
             plt.ion()
             self.fig = plt.figure()
             self._image = plt.imshow(buffer, animated=True)
-            self.fig.canvas.mpl_connect("key_press_event", self.event_handler)
+            self.fig.canvas.mpl_connect("key_press_event", self.key_press_handler)
+            self.fig.canvas.mpl_connect("key_release_event", self.key_release_handler)
         else:
             self._image.set_data(buffer)
         if self.wait_for_button_press:
             plt.waitforbuttonpress()
+        else:
+            self.fig.canvas.draw_idle()
+            self.fig.canvas.flush_events()
         plt.draw()
 
     def __del__(self):
