f061a1d40ae5a17ecbd5c09407d78a948742ea17
Jeremy Morgan <jsmorgan6@gmail.com>
2025-04-19 11:58:25 -0700

    created place-spherev2

--- mani_skill/examples/motionplanning/panda/solutions/__init__.py
+++ mani_skill/examples/motionplanning/panda/solutions/__init__.py
@@ -1,7 +1,6 @@
 from .pick_cube import solve as solvePickCube
 from .stack_cube import solve as solveStackCube
 from .peg_insertion_side import solve as solvePegInsertionSide
-from .place_sphere import solve as solvePlaceSphere
 from .plug_charger import solve as solvePlugCharger
 from .push_cube import solve as solvePushCube
 from .pull_cube_tool import solve as solvePullCubeTool
@@ -9,7 +8,6 @@ from .lift_peg_upright import solve as solveLiftPegUpright
 from .pull_cube import solve as solvePullCube
 from .draw_svg import solve as solveDrawSVG
 from .draw_triangle import solve as solveDrawTriangle
-from .stack_pyramid import solve as solveStackPyramid
 
-from .open_drawer import solve as solveOpenDrawer
-from .raise_cube import solve as solveRaiseCube
+# new
+from .place_sphere import solve as solvePlaceSphere
