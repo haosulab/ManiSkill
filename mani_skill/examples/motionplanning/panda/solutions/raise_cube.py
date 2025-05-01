615ad8f234c4b15f7f37049a82bde29a78317580
Jeremy Morgan <jsmorgan6@gmail.com>
2025-04-30 20:44:48 -0700

    added raisecube-v1, actuually this time

---
+++ mani_skill/examples/motionplanning/panda/solutions/raise_cube.py
@@ -0,0 +1,58 @@
+import numpy as np
+import sapien
+
+from mani_skill.envs.tasks.coloseum_v2 import RaiseCubeEnv
+from mani_skill.examples.motionplanning.panda.motionplanner import PandaArmMotionPlanningSolver
+from mani_skill.examples.motionplanning.panda.utils import compute_grasp_info_by_obb, get_actor_obb
+
+def solve(env: RaiseCubeEnv, seed=None, debug=False, vis=False):
+    env.reset(seed=seed)
+    planner = PandaArmMotionPlanningSolver(
+        env,
+        debug=debug,
+        vis=vis,
+        base_pose=env.unwrapped.agent.robot.pose,
+        visualize_target_grasp_pose=vis,
+        print_env_info=False,
+    )
+
+    FINGER_LENGTH = 0.025
+    env = env.unwrapped
+
+    # retrieves the object oriented bounding box (trimesh box object)
+    obb = get_actor_obb(env.cube)
+
+    approaching = np.array([0, 0, -1])
+    # get transformation matrix of the tcp pose, is default batched and on torch
+    target_closing = env.agent.tcp.pose.to_transformation_matrix()[0, :3, 1].cpu().numpy()
+    # we can build a simple grasp pose using this information for Panda
+    grasp_info = compute_grasp_info_by_obb(
+        obb,
+        approaching=approaching,
+        target_closing=target_closing,
+        depth=FINGER_LENGTH,
+    )
+    closing, center = grasp_info["closing"], grasp_info["center"]
+    grasp_pose = env.agent.build_grasp_pose(approaching, closing, env.cube.pose.sp.p)
+
+    # -------------------------------------------------------------------------- #
+    # Reach
+    # -------------------------------------------------------------------------- #
+    reach_pose = grasp_pose * sapien.Pose([0, 0, -0.05])
+    planner.move_to_pose_with_screw(reach_pose)
+
+    # -------------------------------------------------------------------------- #
+    # Grasp
+    # -------------------------------------------------------------------------- #
+    planner.move_to_pose_with_screw(grasp_pose)
+    planner.close_gripper()
+
+    # -------------------------------------------------------------------------- #
+    # Move to goal pose
+    # -------------------------------------------------------------------------- #
+    target_pose = reach_pose * sapien.Pose([0, 0, 0.3])
+    goal_pose = sapien.Pose(target_pose.p, grasp_pose.q)
+    res = planner.move_to_pose_with_screw(goal_pose)
+
+    planner.close()
+    return res
