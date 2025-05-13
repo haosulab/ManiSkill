17bb8078a88a38c60c43409ebacf47753cfdf4d3
Jeremy Morgan <jmorgan@theaiinstitute.com>
2025-05-12 17:41:24 -0700

    added camera args to random action

--- mani_skill/examples/demo_random_action.py
+++ mani_skill/examples/demo_random_action.py
@@ -26,9 +26,6 @@ class Args:
     sim_backend: Annotated[str, tyro.conf.arg(aliases=["-b"])] = "auto"
     """Which simulation backend to use. Can be 'auto', 'cpu', 'gpu'"""
 
-    render_backend: Annotated[str, tyro.conf.arg(aliases=["-rb"])] = "gpu"
-    """Which render backend to use. Can be 'gpu', 'cpu', 'none'"""
-
     reward_mode: Optional[str] = None
     """Reward mode"""
 
@@ -53,13 +50,16 @@ class Args:
     quiet: bool = False
     """Disable verbose output."""
 
-    seed: Annotated[Optional[Union[int, list[int]]], tyro.conf.arg(aliases=["-s"])] = None
+    seed: Annotated[Optional[Union[int, List[int]]], tyro.conf.arg(aliases=["-s"])] = None
     """Seed(s) for random actions and simulator. Can be a single integer or a list of integers. Default is None (no seeds)"""
 
+    camera_width: int = 640
+    """Camera width"""
+
+    camera_height: int = 480
+    """Camera height"""
 
 def main(args: Args):
-    if args.render_mode == "none":
-        args.render_mode = None
     np.set_printoptions(suppress=True, precision=3)
     verbose = not args.quiet
     if isinstance(args.seed, int):
@@ -82,27 +82,18 @@ def main(args: Args):
         viewer_camera_configs=dict(shader_pack=args.shader),
         num_envs=args.num_envs,
         sim_backend=args.sim_backend,
-        render_backend=args.render_backend,
         enable_shadow=True,
         parallel_in_single_scene=parallel_in_single_scene,
+        camera_width=args.camera_width,
+        camera_height=args.camera_height,
         distraction_set=DistractionSet(),
     )
     if args.robot_uids is not None:
         env_kwargs["robot_uids"] = tuple(args.robot_uids.split(","))
-        if len(env_kwargs["robot_uids"]) == 1:
-            env_kwargs["robot_uids"] = env_kwargs["robot_uids"][0]
-    try:
-        env: BaseEnv = gym.make(
-            args.env_id,
-            **env_kwargs
-        )
-    except TypeError as e:
-        assert "got an unexpected keyword argument 'distraction_set'" in str(e)
-        del env_kwargs["distraction_set"]
-        env = gym.make(
-            args.env_id,
-            **env_kwargs
-        )
+    env: BaseEnv = gym.make(
+        args.env_id,
+        **env_kwargs
+    )
     record_dir = args.record_dir
     if record_dir:
         record_dir = record_dir.format(env_id=args.env_id)
@@ -118,7 +109,7 @@ def main(args: Args):
     obs, _ = env.reset(seed=args.seed, options=dict(reconfigure=True))
     if args.seed is not None and env.action_space is not None:
             env.action_space.seed(args.seed[0])
-    if args.render_mode == "human":
+    if args.render_mode is not None:
         viewer = env.render()
         if isinstance(viewer, sapien.utils.Viewer):
             viewer.paused = args.pause
@@ -131,7 +122,7 @@ def main(args: Args):
             print("terminated", terminated)
             print("truncated", truncated)
             print("info", info)
-        if args.render_mode == "human":
+        if args.render_mode is not None:
             env.render()
         if args.render_mode is None or args.render_mode != "human":
             if (terminated | truncated).any():
