657af80df6b0899a458e945bd1644d444b2ff2dd
Jeremy Morgan <jmorgan@theaiinstitute.com>
2025-05-15 12:24:47 -0700

    first commit

--- mani_skill/examples/motionplanning/panda/solutions/__init__.py
+++ mani_skill/examples/motionplanning/panda/solutions/__init__.py
@@ -1,6 +1,7 @@
 from .pick_cube import solve as solvePickCube
 from .stack_cube import solve as solveStackCube
 from .peg_insertion_side import solve as solvePegInsertionSide
+from .place_sphere import solve as solvePlaceSphere
 from .plug_charger import solve as solvePlugCharger
 from .push_cube import solve as solvePushCube
 from .pull_cube_tool import solve as solvePullCubeTool
