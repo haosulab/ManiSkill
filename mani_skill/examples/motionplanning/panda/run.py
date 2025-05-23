0572850831b666b17c187b949d67aee36b62c795
chenyenru <rebecca946487@gmail.com>
2025-05-22 20:45:18 -0700

    Add StackPyramid env and motionplanning solution

--- mani_skill/examples/motionplanning/panda/run.py
+++ mani_skill/examples/motionplanning/panda/run.py
@@ -9,7 +9,7 @@ from tqdm import tqdm
 import os.path as osp
 from mani_skill.utils.wrappers.record import RecordEpisode
 from mani_skill.trajectory.merge_trajectory import merge_trajectories
-from mani_skill.examples.motionplanning.panda.solutions import solvePushCube, solvePickCube, solveStackCube, solvePegInsertionSide, solvePlugCharger, solvePullCubeTool, solveLiftPegUpright, solvePullCube, solveDrawTriangle, solveDrawSVG, solvePlaceSphere
+from mani_skill.examples.motionplanning.panda.solutions import solvePushCube, solvePickCube, solveStackCube, solvePegInsertionSide, solvePlugCharger, solvePullCubeTool, solveLiftPegUpright, solvePullCube, solveDrawTriangle, solveDrawSVG, solvePlaceSphere, solveStackPyramid
 MP_SOLUTIONS = {
     "DrawTriangle-v1": solveDrawTriangle,
     "PickCube-v1": solvePickCube,
@@ -21,7 +21,8 @@ MP_SOLUTIONS = {
     "PullCubeTool-v1": solvePullCubeTool,
     "LiftPegUpright-v1": solveLiftPegUpright,
     "PullCube-v1": solvePullCube,
-    "DrawSVG-v1" : solveDrawSVG
+    "DrawSVG-v1" : solveDrawSVG,
+    "StackPyramid-v1": solveStackPyramid,
 }
 def parse_args(args=None):
     parser = argparse.ArgumentParser()
