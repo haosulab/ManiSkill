aad75f23da42c0cf30e220bde896d62eb7581e97
StoneT2000 <stonezt2019@gmail.com>
2025-05-27 18:53:54 -0700

    make rl work

--- mani_skill/utils/registration.py
+++ mani_skill/utils/registration.py
@@ -253,6 +253,14 @@ def register_env(
             max_episode_steps=max_episode_steps,
             disable_env_checker=True,  # Temporary solution as we allow empty observation spaces
             kwargs=deepcopy(kwargs),
+            # TODO (stao): why arent custom vec envs allowed to have this?
+            additional_wrappers=[
+                WrapperSpec(
+                    "MSTimeLimit",
+                    entry_point="mani_skill.utils.registration:TimeLimitWrapper",
+                    kwargs=dict(max_episode_steps=max_episode_steps),
+                )
+            ],
         )
 
         return cls
