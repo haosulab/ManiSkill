3b5e2a2f22cb3776e61685f8cfdacb872995be75
StoneT2000 <stonezt2019@gmail.com>
2025-06-13 15:28:07 -0700

    Update ppo_rgb.py

--- examples/baselines/ppo/ppo_rgb.py
+++ examples/baselines/ppo/ppo_rgb.py
@@ -304,15 +304,7 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() and args.cuda else "cpu")
 
     # env setup
-    env_kwargs = dict(
-        obs_mode="rgb+segmentation", render_mode=args.render_mode, sim_backend="physx_cuda",
-        base_camera_settings=dict(
-            pos=[0.69, 0.37, 0.28],
-            fov=0.8256,
-            target=[0.185, -0.15, 0.0]
-        ),
-        greenscreen_overlay_path="backgrounds/greenscreen_background_1.png",
-    )
+    env_kwargs = dict(obs_mode="rgb+segmentation", render_mode=args.render_mode, sim_backend="physx_cuda")
     if args.control_mode is not None:
         env_kwargs["control_mode"] = args.control_mode
     eval_envs = gym.make(args.env_id, num_envs=args.num_eval_envs, reconfiguration_freq=args.eval_reconfiguration_freq, **env_kwargs)
@@ -333,8 +325,8 @@ if __name__ == "__main__":
         if args.save_train_video_freq is not None:
             save_video_trigger = lambda x : (x // args.num_steps) % args.save_train_video_freq == 0
             envs = RecordEpisode(envs, output_dir=f"runs/{run_name}/train_videos", save_trajectory=False, save_video_trigger=save_video_trigger, max_steps_per_video=args.num_steps, video_fps=30)
-        eval_envs = RecordEpisode(eval_envs, output_dir=eval_output_dir, save_trajectory=args.evaluate, trajectory_name="trajectory", max_steps_per_video=args.num_eval_steps, video_fps=30, info_on_video=True)
-    envs = ManiSkillVectorEnv(envs, args.num_envs, ignore_terminations=not args.partial_reset, record_metrics=True, cached_resets=args.cached_resets)
+        eval_envs = RecordEpisode(eval_envs, output_dir=eval_output_dir, save_trajectory=args.evaluate, trajectory_name="trajectory", max_steps_per_video=args.num_eval_steps, video_fps=30)
+    envs = ManiSkillVectorEnv(envs, args.num_envs, ignore_terminations=not args.partial_reset, record_metrics=True)
     eval_envs = ManiSkillVectorEnv(eval_envs, args.num_eval_envs, ignore_terminations=not args.eval_partial_reset, record_metrics=True)
     assert isinstance(envs.single_action_space, gym.spaces.Box), "only continuous action space is supported"
 
