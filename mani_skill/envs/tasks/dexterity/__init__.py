36487bba54e394196442ff0deb5ca68649162357
liuyulinn <19-liuyulin@pku.edu.cn>
2025-06-18 22:05:53 -0700

    insert flower

--- mani_skill/envs/tasks/dexterity/__init__.py
+++ mani_skill/envs/tasks/dexterity/__init__.py
@@ -1,3 +1,6 @@
 # isort: off
 from .rotate_valve import RotateValveEnv
 from .rotate_single_object_in_hand import RotateSingleObjectInHand
+from .open_notebook import OpenNotebookEnv
+from .pour_water import PourWaterEnv
+from .insert_flower import InsertFlowerEnv
