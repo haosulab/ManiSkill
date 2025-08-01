5f0d71f07b31f6dd1a161d7b995e49982752ee68
suk063 <suk063@ucsd.edu>
2025-08-01 16:23:40 -0700

    chore: image rl baseline

--- map_rl/model.py
+++ map_rl/model.py
@@ -178,6 +178,65 @@ class MapAwareFeatureExtractor(nn.Module):
         return torch.cat(encoded_tensor_list, dim=1)
 
 
+class NatureCNN(nn.Module):
+    def __init__(self, sample_obs):
+        super().__init__()
+
+        extractors = {}
+
+        self.out_features = 0
+        feature_size = 256
+        in_channels=sample_obs["rgb"].shape[-1]
+        image_size=(sample_obs["rgb"].shape[1], sample_obs["rgb"].shape[2])
+
+
+        # here we use a NatureCNN architecture to process images, but any architecture is permissble here
+        cnn = nn.Sequential(
+            nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=32,
+                kernel_size=8,
+                stride=4,
+                padding=0,
+            ),
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels=32, out_channels=64, kernel_size=4, stride=2, padding=0
+            ),
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=0
+            ),
+            nn.ReLU(),
+            nn.Flatten(),
+        )
+
+        # to easily figure out the dimensions after flattening, we pass a test tensor
+        with torch.no_grad():
+            n_flatten = cnn(sample_obs["rgb"].float().permute(0,3,1,2).cpu()).shape[1]
+            fc = nn.Sequential(nn.Linear(n_flatten, feature_size), nn.ReLU())
+        extractors["rgb"] = nn.Sequential(cnn, fc)
+        self.out_features += feature_size
+
+        if "state" in sample_obs:
+            # for state data we simply pass it through a single linear layer
+            state_size = sample_obs["state"].shape[-1]
+            extractors["state"] = nn.Linear(state_size, 256)
+            self.out_features += 256
+
+        self.extractors = nn.ModuleDict(extractors)
+
+    def forward(self, observations, map_features: Optional[List] = None) -> torch.Tensor:
+        encoded_tensor_list = []
+        # self.extractors contain nn.Modules that do all the processing.
+        for key, extractor in self.extractors.items():
+            obs = observations[key]
+            if key == "rgb":
+                obs = obs.float().permute(0,3,1,2)
+                obs = obs / 255
+            encoded_tensor_list.append(extractor(obs))
+        return torch.cat(encoded_tensor_list, dim=1)
+
 class CNN(nn.Module):
     def __init__(self, in_channels):
         super().__init__()
@@ -262,6 +321,7 @@ class Agent(nn.Module):
     def __init__(self, envs, sample_obs, decoder: Optional[nn.Module] = None):
         super().__init__()
         self.feature_net = MapAwareFeatureExtractor(sample_obs=sample_obs, decoder=decoder)
+        # self.feature_net = NatureCNN(sample_obs=sample_obs)
         latent_size = self.feature_net.out_features
         
         # Critic
