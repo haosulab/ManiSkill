f1cbedc4355e822177d09828c2921d2ee384bb69
suk063 <suk063@ucsd.edu>
2025-08-08 06:14:21 -0700

    chore: remove unused files

--- scripts/data_generation/learning_from_demos.sh
+++
@@ -1,1 +0,0 @@
-# Learning from demonstrations based approach to learn a policy from a few demonstrations to then rollout more successful demonstrations
---
+++ scripts/map_rl.sh
@@ -0,0 +1,14 @@
+python map_rl/train_ppo.py \
+    --env_id=PickCubeDiscreteInit-v1 \
+    --robot_uids=xarm6_robotiq \
+    --control_mode=pd_joint_vel \
+    --exp_name=PickCube_xarm6_ppo \
+    --num_envs=50 \
+    --num_eval_envs=20 \
+    --eval_freq=20 \
+    --total_timesteps=100_000_000 \
+    --num_steps=100 \
+    --gamma=0.9 \
+    --capture-video \
+    --track \
+    --wandb_project_name "ManiSkill-RL"
