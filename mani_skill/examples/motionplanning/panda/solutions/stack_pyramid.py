eeabb11b012c42f52603ea997ce182d69a7e98d1
cmcgartoll <cmcgartoll@gmail.com>
2025-08-26 23:03:50 -0700

    Interactive so100 + xarm6, creation of base_motionplanner to abstract functionality from other motionplanners

--- mani_skill/examples/motionplanning/panda/solutions/stack_pyramid.py
+++ mani_skill/examples/motionplanning/panda/solutions/stack_pyramid.py
@@ -7,7 +7,7 @@ from transforms3d.euler import euler2quat
 from mani_skill.envs.tasks import StackPyramidEnv
 from mani_skill.examples.motionplanning.panda.motionplanner import \
     PandaArmMotionPlanningSolver
-from mani_skill.examples.motionplanning.panda.utils import (
+from mani_skill.examples.motionplanning.base_motionplanner.utils import (
     compute_grasp_info_by_obb, get_actor_obb)
 from mani_skill.utils.wrappers.record import RecordEpisode
 from mani_skill.utils.structs import Pose
--- mani_skill/examples/motionplanning/xarm6/solutions/plug_charger.py
+++ mani_skill/examples/motionplanning/xarm6/solutions/plug_charger.py
@@ -8,7 +8,7 @@ from transforms3d.euler import euler2quat
 from mani_skill.envs.tasks import PlugChargerEnv
 from mani_skill.examples.motionplanning.xarm6.motionplanner import \
     XArm6RobotiqMotionPlanningSolver, XArm6PandaGripperMotionPlanningSolver
-from mani_skill.examples.motionplanning.panda.utils import (
+from mani_skill.examples.motionplanning.base_motionplanner.utils import (
     compute_grasp_info_by_obb, get_actor_obb)
 
 
