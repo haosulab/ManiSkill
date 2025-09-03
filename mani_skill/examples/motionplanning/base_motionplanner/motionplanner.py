1809d125f869aa6bb05bf1d7422b78ec73a6a4be
cmcgartoll <cmcgartoll@gmail.com>
2025-09-02 22:58:13 -0700

    imports cleanup

--- mani_skill/examples/motionplanning/base_motionplanner/motionplanner.py
+++ mani_skill/examples/motionplanning/base_motionplanner/motionplanner.py
@@ -5,11 +5,7 @@ import trimesh
 
 from mani_skill.agents.base_agent import BaseAgent
 from mani_skill.envs.sapien_env import BaseEnv
-from mani_skill.envs.scene import ManiSkillScene
 from mani_skill.utils.structs.pose import to_sapien_pose
-import sapien.physx as physx
-from transforms3d import quaternions
-from mani_skill.envs.scene import ManiSkillScene
 
 
 class BaseMotionPlanningSolver:
