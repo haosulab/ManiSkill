ca21f3cb771d2b7c2a7947a070290e9a69ceef39
Jeremy Morgan <jsmorgan6@gmail.com>
2025-10-21 17:32:24 -0700

    first commit (#7)

--- mani_skill/examples/motionplanning/panda/solutions/open_drawer.py
+++ mani_skill/examples/motionplanning/panda/solutions/open_drawer.py
@@ -5,7 +5,7 @@ from transforms3d.euler import euler2quat
 from mani_skill.envs.tasks import OpenDrawerV1Env
 from mani_skill.utils import common
 from mani_skill.examples.motionplanning.panda.motionplanner import PandaArmMotionPlanningSolver
-from mani_skill.examples.motionplanning.panda.utils import compute_grasp_info_by_obb, get_actor_obb
+from mani_skill.examples.motionplanning.base_motionplanner.utils import compute_grasp_info_by_obb, get_actor_obb
 from mani_skill.utils.geometry.rotation_conversions import quaternion_multiply
 
 def solve(env: OpenDrawerV1Env, seed=None, debug=False, vis=False):
