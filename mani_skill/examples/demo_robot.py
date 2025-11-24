a598e4a04b1c0ca728d7398473616b5148047df6
StoneT2000 <stonezt2019@gmail.com>
2025-11-24 00:49:25 -0800

    add tyro

--- mani_skill/examples/demo_robot.py
+++ mani_skill/examples/demo_robot.py
@@ -2,36 +2,29 @@
 Instantiates a empty environment with a floor, and attempts to place any given robot in there
 """
 
-import argparse
-
+from dataclasses import dataclass
+from typing import Annotated, Optional
+import tyro
 import gymnasium as gym
 import mani_skill
 from mani_skill.agents.controllers.base_controller import DictController
 from mani_skill.envs.sapien_env import BaseEnv
-def parse_args(args=None):
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-r", "--robot-uid", type=str, default="panda", help="The id of the robot to place in the environment")
-    parser.add_argument("-b", "--sim-backend", type=str, default="auto", help="Which simulation backend to use. Can be 'auto', 'cpu', 'gpu'")
-    parser.add_argument("-c", "--control-mode", type=str, default="pd_joint_pos", help="The control mode to use. Note that for new robots being implemented if the _controller_configs is not implemented in the selected robot, we by default provide two default controllers, 'pd_joint_pos' and 'pd_joint_delta_pos' ")
-    parser.add_argument("-k", "--keyframe", type=str, help="The name of the keyframe of the robot to display")
-    parser.add_argument("--shader", default="default", type=str, help="Change shader used for rendering. Default is 'default' which is very fast. Can also be 'rt' for ray tracing and generating photo-realistic renders. Can also be 'rt-fast' for a faster but lower quality ray-traced renderer")
-    parser.add_argument("--keyframe-actions", action="store_true", help="Whether to use the selected keyframe to set joint targets to try and hold the robot in its position")
-    parser.add_argument("--random-actions", action="store_true", help="Whether to sample random actions to control the agent. If False, no control signals are sent and it is just rendering.")
-    parser.add_argument("--none-actions", action="store_true", help="If set, then the scene and rendering will update each timestep but no joints will be controlled via code. You can use this to control the robot freely via the GUI.")
-    parser.add_argument("--zero-actions", action="store_true", help="Whether to send zero actions to the robot. If False, no control signals are sent and it is just rendering.")
-    parser.add_argument("--sim-freq", type=int, default=100, help="Simulation frequency")
-    parser.add_argument("--control-freq", type=int, default=20, help="Control frequency")
-    parser.add_argument(
-        "-s",
-        "--seed",
-        type=int,
-        help="Seed the random actions and environment. Default is no seed",
-    )
-    args = parser.parse_args()
-    return args
+@dataclass
+class Args:
+    robot_uid: Annotated[str, tyro.conf.arg(aliases=["-r"])] = "panda"
+    sim_backend: Annotated[str, tyro.conf.arg(aliases=["-b"])] = "auto"
+    control_mode: Annotated[str, tyro.conf.arg(aliases=["-c"])] = "pd_joint_pos"
+    keyframe: Annotated[Optional[str], tyro.conf.arg(aliases=["-k"])] = None
+    shader: str = "default"
+    keyframe_actions: bool = False
+    random_actions: bool = False
+    none_actions: bool = False
+    zero_actions: bool = False
+    sim_freq: int = 100
+    control_freq: int = 20
+    seed: Annotated[Optional[int], tyro.conf.arg(aliases=["-s"])] = None
 
-def main():
-    args = parse_args()
+def main(args: Args):
     env = gym.make(
         "Empty-v1",
         obs_mode="none",
@@ -93,4 +86,4 @@ def main():
         viewer = env.render()
 
 if __name__ == "__main__":
-    main()
+    main(tyro.cli(Args))
