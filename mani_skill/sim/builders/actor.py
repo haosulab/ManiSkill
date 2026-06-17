4c9aaef2a8939028492348f7a7d090b41853691f
Stone Tao <stonezt2019@gmail.com>
2026-06-17 07:34:21 -0700

    Update actor.py

--- mani_skill/sim/builders/actor.py
+++ mani_skill/sim/builders/actor.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Any
 
+import sapien
+
 from mani_skill.sim.builders.base_builder import BaseBuilder
 from mani_skill.utils.structs.actor import Actor
 from mani_skill.utils.structs.pose import Pose
@@ -74,6 +76,14 @@ class BaseActorBuilder(BaseBuilder):
         self._sims.pop(sim.id)
         return self
 
+    def set_initial_pose(self, initial_pose: Pose | None = None):
+        """
+        Set the initial pose of the actor. This is the pose of the actor when it is built and 
+        spawned into the simulation before any physics steps are taken.
+        """
+        self.initial_pose = initial_pose
+        return self
+
     def set_scene_idxs(self, scene_idxs: list[int] | None = None):
         """
         Set the sub-scene indices (parallel environment IDs) to build this actor in.
@@ -98,7 +108,14 @@ class BaseActorBuilder(BaseBuilder):
         Returns:
             The built actor.
         """
-        raise NotImplementedError()
+        for sim in self._sims.values():
+            if type(self._sim_builders[sim.id]) is BaseActorBuilder:
+                raise NotImplementedError(
+                    f"{self._sim_builders[sim.id].__class__.__name__} does not support "
+                    "build."
+                )
+            actor = self._sim_builders[sim.id].build(name=name)
+        return actor
 
     def build_kinematic(self, name: str) -> Actor:
         """
@@ -116,13 +133,155 @@ class BaseActorBuilder(BaseBuilder):
                     f"{self._sim_builders[sim.id].__class__.__name__} does not support "
                     "build_kinematic."
                 )
-            self._sim_builders[sim.id].build_kinematic(name=name)
-        return self
+            actor = self._sim_builders[sim.id].build_kinematic(name=name)
+        return actor
 
     ### Standard primitive building functions, based on Sapien's original ActorBuilder ###
+    def add_plane_collision(
+        self,
+        pose: Pose | None = None,
+        material: Any | None = None,
+        patch_radius: float = 0,
+        min_patch_radius: float = 0,
+    ):
+        for sim in self._sims.values():
+            if type(self._sim_builders[sim.id]) is BaseActorBuilder:
+                raise NotImplementedError(
+                    f"{self._sim_builders[sim.id].__class__.__name__} does not support "
+                    "add_plane_collision."
+                )
+            self._sim_builders[sim.id].add_plane_collision(
+                pose=pose if pose is not None else sapien.Pose(),
+                material=material,
+                patch_radius=patch_radius,
+                min_patch_radius=min_patch_radius,
+            )
+        return self
+
+    def add_plane_visual(
+        self,
+        pose: Pose | None = None,
+        scale: Vec3 = (1, 1, 1),
+        material: Any | Vec3 | None = None,
+        name: str = "",
+    ):
+        for sim in self._sims.values():
+            if type(self._sim_builders[sim.id]) is BaseActorBuilder:
+                raise NotImplementedError(
+                    f"{self._sim_builders[sim.id].__class__.__name__} does not support "
+                    "add_plane_visual."
+                )
+            self._sim_builders[sim.id].add_plane_visual(
+                pose=pose if pose is not None else sapien.Pose(),
+                scale=scale,
+                material=material,
+                name=name,
+            )
+        return self
+
+    def add_capsule_collision(
+        self,
+        pose: Pose | None = None,
+        radius: float = 1,
+        half_length: float = 1,
+        material: Any | None = None,
+        density: float = 1000,
+        patch_radius: float = 0,
+        min_patch_radius: float = 0,
+    ):
+        for sim in self._sims.values():
+            if type(self._sim_builders[sim.id]) is BaseActorBuilder:
+                raise NotImplementedError(
+                    f"{self._sim_builders[sim.id].__class__.__name__} does not support "
+                    "add_capsule_collision."
+                )
+            self._sim_builders[sim.id].add_capsule_collision(
+                pose=pose if pose is not None else sapien.Pose(),
+                radius=radius,
+                half_length=half_length,
+                material=material,
+                density=density,
+                patch_radius=patch_radius,
+                min_patch_radius=min_patch_radius,
+            )
+        return self
+    
+    def add_capsule_visual(
+        self,
+        pose: Pose | None = None,
+        radius: float = 1,
+        half_length: float = 1,
+        material: Any | Vec3 | None = None,
+        name: str = "",
+    ):
+        for sim in self._sims.values():
+            if type(self._sim_builders[sim.id]) is BaseActorBuilder:
+                raise NotImplementedError(
+                    f"{self._sim_builders[sim.id].__class__.__name__} does not support "
+                    "add_capsule_visual."
+                )
+            self._sim_builders[sim.id].add_capsule_visual(
+                pose=pose if pose is not None else sapien.Pose(),
+                radius=radius,
+                half_length=half_length,
+                material=material,
+                name=name,
+            )
+        return self
+
+    def add_cylinder_collision(
+        self,
+        pose: Pose | None = None,
+        radius: float = 1,
+        half_length: float = 1,
+        material: Any | None = None,
+        density: float = 1000,
+        patch_radius: float = 0,
+        min_patch_radius: float = 0,
+    ):
+        for sim in self._sims.values():
+            if type(self._sim_builders[sim.id]) is BaseActorBuilder:
+                raise NotImplementedError(
+                    f"{self._sim_builders[sim.id].__class__.__name__} does not support "
+                    "add_cylinder_collision."
+                )
+            self._sim_builders[sim.id].add_cylinder_collision(
+                pose=pose if pose is not None else sapien.Pose(),
+                radius=radius,
+                half_length=half_length,
+                material=material,
+                density=density,
+                patch_radius=patch_radius,
+                min_patch_radius=min_patch_radius,
+            )
+        return self
+
+    def add_cylinder_visual(
+        self,
+        pose: Pose | None = None,
+        radius: float = 1,
+        half_length: float = 1,
+        material: Any | Vec3 | None = None,
+        name: str = "",
+    ):
+        for sim in self._sims.values():
+            if type(self._sim_builders[sim.id]) is BaseActorBuilder:
+                raise NotImplementedError(
+                    f"{self._sim_builders[sim.id].__class__.__name__} does not support "
+                    "add_cylinder_visual."
+                )
+            self._sim_builders[sim.id].add_cylinder_visual(
+                pose=pose if pose is not None else sapien.Pose(),
+                radius=radius,
+                half_length=half_length,
+                material=material,
+                name=name,
+            )
+        return self
+
     def add_box_collision(
         self,
-        pose: Pose,
+        pose: Pose | None = None,
         half_size: Vec3 = (1.0, 1.0, 1.0),
         material: Any | None = None,
         density: float = 1000.0,
@@ -149,7 +308,7 @@ class BaseActorBuilder(BaseBuilder):
                     "add_box_collision."
                 )
             self._sim_builders[sim.id].add_box_collision(
-                pose=pose,
+                pose=pose if pose is not None else sapien.Pose(),
                 half_size=half_size,
                 material=material,
                 density=density,
@@ -158,9 +317,10 @@ class BaseActorBuilder(BaseBuilder):
 
     def add_box_visual(
         self,
-        pose: Pose,
+        pose: Pose | None = None,
         half_size: Vec3 = (1.0, 1.0, 1.0),
         material: Any | Vec3 | None = None,
+        name: str = "",
     ) -> "BaseActorBuilder":
         # TODO (stao): check Newton API here w.r.t concept of material config
         """
@@ -172,15 +332,28 @@ class BaseActorBuilder(BaseBuilder):
             material: The material of the box. This is dependent on simulator backend used.
             For SAPIEN this is a `sapien.render.RenderMaterial` object.
             For Newton based backends this is a ShapeCfg object.
+            name: The name of the box visual.
 
         Returns:
             The actor builder.
         """
-        raise NotImplementedError("")
+        for sim in self._sims.values():
+            if type(self._sim_builders[sim.id]) is BaseActorBuilder:
+                raise NotImplementedError(
+                    f"{self._sim_builders[sim.id].__class__.__name__} does not support "
+                    "add_box_visual."
+                )
+            self._sim_builders[sim.id].add_box_visual(
+                pose=pose if pose is not None else sapien.Pose(),
+                half_size=half_size,
+                material=material,
+                name=name,
+            )
+        return self
 
     def add_sphere_collision(
         self,
-        pose: Pose,
+        pose: Pose | None = None,
         radius: float = 1.0,
         material: Any | Vec3 | None = None,
         density: float = 1000.0,
@@ -199,13 +372,26 @@ class BaseActorBuilder(BaseBuilder):
         Returns:
             The actor builder.
         """
-        raise NotImplementedError("")
+        for sim in self._sims.values():
+            if type(self._sim_builders[sim.id]) is BaseActorBuilder:
+                raise NotImplementedError(
+                    f"{self._sim_builders[sim.id].__class__.__name__} does not support "
+                    "add_sphere_collision."
+                )
+            self._sim_builders[sim.id].add_sphere_collision(
+                pose=pose if pose is not None else sapien.Pose(),
+                radius=radius,
+                material=material,
+                density=density,
+            )
+        return self
 
     def add_sphere_visual(
         self,
         pose: Pose,
         radius: float = 1.0,
         material: Any | Vec3 | None = None,
+        name: str = "",
     ) -> "BaseActorBuilder":
         """
         Add a sphere visual to the actor.
@@ -216,11 +402,24 @@ class BaseActorBuilder(BaseBuilder):
             material: The material of the sphere. This is dependent on simulator backend used.
             For SAPIEN this is a `sapien.render.RenderMaterial` object.
             For Newton based backends this is a ShapeCfg object.
+            name: The name of the sphere visual.
 
         Returns:
             The actor builder.
         """
-        raise NotImplementedError("")
+        for sim in self._sims.values():
+            if type(self._sim_builders[sim.id]) is BaseActorBuilder:
+                raise NotImplementedError(
+                    f"{self._sim_builders[sim.id].__class__.__name__} does not support "
+                    "add_sphere_visual."
+                )
+            self._sim_builders[sim.id].add_sphere_visual(
+                pose=pose if pose is not None else sapien.Pose(),
+                radius=radius,
+                material=material,
+                name=name,
+            )
+        return self
 
     # def add_convex_collision_from_file(
     #     self,
@@ -237,6 +436,68 @@ class BaseActorBuilder(BaseBuilder):
     #     Add a convex collision from a file to the actor.
     #     """
     #     raise NotImplementedError("")
+
+    # def add_multiple_convex_collisions_from_file(
+    #     self,
+    #     filename,
+    #     pose: sapien.Pose = sapien.Pose(),
+    #     scale: Vec3 = (1, 1, 1),
+    #     material: Union[sapien.physx.PhysxMaterial, None] = None,
+    #     density: float = 1000,
+    #     patch_radius: float = 0,
+    #     min_patch_radius: float = 0,
+    #     is_trigger: bool = False,
+    #     decomposition: typing.Literal["none", "coacd"] = "none",
+    #     decomposition_params=dict(),
+    # ):
+    #     if material is None:
+    #         material = sapien.physx.get_default_material()
+
+    #     self.collision_records.append(
+    #         CollisionShapeRecord(
+    #             type="multiple_convex_meshes",
+    #             filename=filename,
+    #             pose=pose,
+    #             scale=scale,
+    #             material=material,
+    #             density=density,
+    #             patch_radius=patch_radius,
+    #             min_patch_radius=min_patch_radius,
+    #             is_trigger=is_trigger,
+    #             decomposition=decomposition,
+    #             decomposition_params=decomposition_params,
+    #         )
+    #     )
+    #     return self
+
+    # def add_nonconvex_collision_from_file(
+    #     self,
+    #     filename: str,
+    #     pose: sapien.Pose = sapien.Pose(),
+    #     scale: Vec3 = (1, 1, 1),
+    #     material: Union[sapien.physx.PhysxMaterial, None] = None,
+    #     density: float = 1000,
+    #     patch_radius: float = 0,
+    #     min_patch_radius: float = 0,
+    #     is_trigger: bool = False,
+    # ):
+    #     if material is None:
+    #         material = sapien.physx.get_default_material()
+
+    #     self.collision_records.append(
+    #         CollisionShapeRecord(
+    #             type="nonconvex_mesh",
+    #             filename=filename,
+    #             pose=pose,
+    #             scale=scale,
+    #             material=material,
+    #             density=density,
+    #             patch_radius=patch_radius,
+    #             min_patch_radius=min_patch_radius,
+    #             is_trigger=is_trigger,
+    #         )
+    #     )
+    #     return self
 
     def add_visual_from_file(
         self,
@@ -255,6 +516,9 @@ class BaseActorBuilder(BaseBuilder):
             scale: The scale of the visual mesh.
             material: The material of the visual mesh. This is dependent on simulator backend used.
             name: The name of the visual mesh.
+
+        Returns:
+            The actor builder.
         """
         for sim in self._sims.values():
             if type(self._sim_builders[sim.id]) is BaseActorBuilder:
