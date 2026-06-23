f5f2ffcf754c5a9a4189bb7e9e052c1894c35192
Stone Tao <stonezt2019@gmail.com>
2026-06-22 19:13:18 -0700

    fix link issues

--- mani_skill/envs/tasks/mobile_manipulation/open_cabinet_drawer.py
+++ mani_skill/envs/tasks/mobile_manipulation/open_cabinet_drawer.py
@@ -22,8 +22,6 @@ from mani_skill.utils.structs.types import GPUMemoryConfig, SimConfig
 CABINET_COLLISION_BIT = 29
 
 
-# TODO (stao): we need to cut the meshes of all the cabinets in this dataset for gpu sim, there may be some wierd physics
-# that may happen although it seems okay for state based RL
 @register_env(
     "OpenCabinetDrawer-v1",
     asset_download_ids=["partnet_mobility_cabinet"],
@@ -32,20 +30,25 @@ CABINET_COLLISION_BIT = 29
 class OpenCabinetDrawerEnv(BaseEnv):
     """
     **Task Description:**
-    Use the Fetch mobile manipulation robot to move towards a target cabinet and open the target drawer out.
+    Use the Fetch mobile manipulation robot to move towards a target cabinet and open the
+    target drawer out.
 
     **Randomizations:**
-    - Robot is randomly initialized 1.6 to 1.8 meters away from the cabinet and positioned to face it
+    - Robot is randomly initialized 1.6 to 1.8 meters away from the cabinet and positioned
+      to face it
     - Robot's base orientation is randomized by -9 to 9 degrees
-    - The cabinet selected to manipulate is randomly sampled from all PartnetMobility cabinets that have drawers
+    - The cabinet selected to manipulate is randomly sampled from all PartnetMobility cabinets
+      that have drawers
     - The drawer to open is randomly sampled from all drawers available to open
 
     **Success Conditions:**
-    - The drawer is open at least 90% of the way, and the angular/linear velocities of the drawer link are small
+    - The drawer is open at least 90% of the way, and the angular/linear velocities of the
+      drawer link are small
 
     **Goal Specification:**
-    - 3D goal position is defined as the center of mass of the handle mesh on the drawer
-      to open. In human renders, this goal is visualized with a sphere.
+    - 3D goal position is defined as the center of mass of the handle mesh on the drawer to
+      open. In human renders, this goal is visualized with a sphere.
+
     """
 
     _sample_video_link = "https://github.com/mani-skill/ManiSkill/raw/main/figures/environment_demos/OpenCabinetDrawer-v1_rt.mp4"
